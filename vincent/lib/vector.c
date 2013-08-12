6aba8afb714074782fe61c0ff82995ca20cc87d9
Vincent Schüßler <vincent@vschuessler.org>
2013-08-12 11:55:04 +0200

    Use vec3_new() in vector lib

--- vincent/lib/vector.c
+++ vincent/lib/vector.c
@@ -24,21 +24,19 @@ vec3 vec3_normalize(vec3 v) {
 }
 
 vec3 vec3_inv(vec3 v) {
-	vec3 w = {
-		.x = 1. / v.x,
-		.y = 1. / v.y,
-		.z = 1. / v.z,
-	};
-	return w;
+	return vec3_new(
+		1. / v.x,
+		1. / v.y,
+		1. / v.z
+	);
 }
 
 vec3 vec3_add(vec3 v, vec3 w) {
-	vec3 u = {
-		.x = v.x + w.x,
-		.y = v.y + w.y,
-		.z = v.z + w.z,
-	};
-	return u;
+	return vec3_new(
+		v.x + w.x,
+		v.y + w.y,
+		v.z + w.z
+	);
 }
 
 vec3 vec3_sub(vec3 v, vec3 w) {
@@ -46,12 +44,11 @@ vec3 vec3_sub(vec3 v, vec3 w) {
 }
 
 vec3 vec3_mult(vec3 v, vec3 w) {
-	vec3 u = {
-		.x = v.x * w.x,
-		.y = v.y * w.y,
-		.z = v.z * w.z,
-	};
-	return u;
+	return vec3_new(
+		v.x * w.x,
+		v.y * w.y,
+		v.z * w.z
+	);
 }
 
 vec3 vec3_div(vec3 v, vec3 w) {
@@ -59,30 +56,27 @@ vec3 vec3_div(vec3 v, vec3 w) {
 }
 
 vec3 vec3_s_add(float s, vec3 v) {
-	vec3 u = {
-		.x = v.x + s,
-		.y = v.y + s,
-		.z = v.z + s,
-	};
-	return u;
+	return vec3_new(
+		v.x + s,
+		v.y + s,
+		v.z + s
+	);
 }
 
 vec3 vec3_s_mult(float s, vec3 v) {
-	vec3 u = {
-		.x = v.x * s,
-		.y = v.y * s,
-		.z = v.z * s,
-	};
-	return u;
+	return vec3_new(
+		v.x * s,
+		v.y * s,
+		v.z * s
+	);
 }
 
 vec3 vec3_cross(vec3 v, vec3 w) {
-	vec3 u = {
-		.x = v.y * w.z - v.z * w.y,
-		.y = v.z * w.x - v.x * w.z,
-		.z = v.x * w.y - v.y * w.x,
-	};
-	return u;
+	return vec3_new(
+		v.y * w.z - v.z * w.y,
+		v.z * w.x - v.x * w.z,
+		v.x * w.y - v.y * w.x
+	);
 }
 
 vec3 vec3_rotate(vec3 v, vec3 a, float angle) {
