faa22417860ce9afba5ac250aef3ab8880175c06
Vincent Schüßler <vincent@vschuessler.org>
2013-09-06 13:45:44 +0200

    Add nlerp and lerp functions to libzeuch

--- libzeuch/quaternion.c
+++ libzeuch/quaternion.c
@@ -36,3 +36,10 @@ static quat quat_conjugate(quat q) {
 	q.v = vec3_s_mult(-1., q.v);
 	return q;
 }
+
+quat quat_nlerp(quat q, quat r, float t) {
+	q.v = vec3_lerp(q.v, r.v, t);
+	q.w = q.w * (1. - t) + r.w * t;
+	quat_normalize(q);
+	return q;
+}
