a485b0bb394102b493056e37fa265f53870fb0c0
Vincent Schüßler <vincent@vschuessler.org>
2013-09-19 20:38:39 +0200

    Rename oscillator parameter to t and make it const

--- libkrach/osc.c
+++ libkrach/osc.c
@@ -3,29 +3,29 @@
 
 #include "osc.h"
 
-static float fract(float x);
+static float fract(float t);
 
-float osc_sin(float x) {
+float osc_sin(const float t) {
 	const float TAU = 6.283185;
-	return sin(TAU * x);
+	return sin(TAU * t);
 }
 
-float osc_tri(float x) {
-	return 4. * fabs(fract(x) - .5) - 1.;
+float osc_tri(const float t) {
+	return 4. * fabs(fract(t) - .5) - 1.;
 }
 
-float osc_saw(float x) {
-	return 2. * fract(x) - 1.;
+float osc_saw(const float t) {
+	return 2. * fract(t) - 1.;
 }
 
-float osc_rect(float x) {
-	return 2. * signbit(fract(x) - .5) - 1.;
+float osc_rect(const float t) {
+	return 2. * signbit(fract(t) - .5) - 1.;
 }
 
 float osc_noise(void) {
 	return 2. * ((float) rand() / RAND_MAX) - 1.;
 }
 
-static float fract(float x) {
-	return x - floorf(x);
+static float fract(const float t) {
+	return t - floorf(t);
 }
