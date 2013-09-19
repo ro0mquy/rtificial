bb9b968f4a7742f6ffe885d3386890563600478e
Vincent Schüßler <vincent@vschuessler.org>
2013-09-19 20:38:39 +0200

    Rename oscillator parameter to t and make it const

--- libkrach/osc.h
+++ libkrach/osc.h
@@ -1,13 +1,13 @@
 #ifndef OSC_H
 #define OSC_H
 
-float osc_sin(float x);
+float osc_sin(float t);
 
-float osc_tri(float x);
+float osc_tri(float t);
 
-float osc_saw(float x);
+float osc_saw(float t);
 
-float osc_rect(float x);
+float osc_rect(float t);
 
 float osc_noise(void);
 
