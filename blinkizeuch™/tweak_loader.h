b24f14e3490004eb3952162f8d1e1e0fe9c79595
Vincent Schüßler <vincent@vschuessler.org>
2013-09-28 17:27:59 +0200

    Create functions for tweakables

--- blinkizeuch™/tweak_loader.h
+++ blinkizeuch™/tweak_loader.h
@@ -1,17 +1,7 @@
 #ifndef TWEAK_LOADER_H
 #define TWEAK_LOADER_H
 
-typedef enum tweakable_type_t {
-	COLOR, FLOAT
-} tweakable_type_t;
-
-typedef struct tweakable_t {
-	char* name;
-	char* uniform_name;
-	tweakable_type_t type;
-	void* value;
-
-} tweakable_t;
+#include "tweakable.h"
 
 int tweak_loader_load(const char filename[], tweakable_t** tweakables_out);
 
