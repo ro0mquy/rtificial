4898faced41bb918fbcef771025a47ea87583a97
Vincent Schüßler <vincent@vschuessler.org>
2013-09-29 15:23:49 +0200

    Port camera_test to libblink

--- blinkizeuch™/scenes/camera_test/scene.glsl
+++ blinkizeuch™/scenes/camera_test/scene.glsl
@@ -1,53 +1,36 @@
-uniform vec3 viewPosition;
-uniform vec3 viewUp;
-uniform vec3 viewDirection;
-uniform vec2 res;
-uniform float time;
-
 uniform vec3 floor_color1;
 uniform vec3 floor_color2;
 uniform vec3 sphere1_color;
 uniform vec3 sphere2_color;
-uniform vec3 glow_color;
 
 uniform sampler2D tex1;
 uniform sampler2D tex2;
 
 vec2 f(vec3 p);
-vec3 calcNormal(vec3 p);
 float cubicPulse(float c, float w, float x);
-vec2 min_material(vec2 a, vec2 b);
+
+DECLARE_MARCH(march)
+DECLARE_NORMAL(calc_normal)
+
+DEFINE_MARCH(march, f)
+DEFINE_NORMAL(calc_normal, f)
 
 vec3 colors[4];
 
 void main(void) {
-	vec3 viewRight = cross(viewDirection, viewUp);
-	mat3 camera = mat3(viewRight, viewUp, -viewDirection);
-	vec3 direction = camera * normalize(vec3((gl_FragCoord.xy - .5 * res) / res.y , -1.));
+	vec3 direction = get_direction();
 
 	colors[0] = sphere1_color;
 	colors[1] = sphere2_color;
 	colors[3] = vec3(.03, .0, .0);
 
-	vec3 p = viewPosition;
-	int i = 0;
-	float walked = 0.;
-	float a = 0.;
-	for(i = 0; i < 100; i++) {
-		float dist = f(p).x;
-		p += direction * dist;
-
-		dist = abs(dist);
-		float j = float(i) / 100.;
-		a += smoothstep(0., .2, dist) * j * j;
-		walked += dist;
-		if(dist < .001 * walked) break;
-	}
+	int i;
+	vec3 p = march(view_position, direction, i);
 
 	vec3 color = vec3(0.);
 	if(i < 100) {
 		int material = int(f(p).y);
-		vec3 normal = calcNormal(p);
+		vec3 normal = calc_normal(p);
 		vec4 l = max(normal * mat4x3(
 			normalize(vec3(2.1, -.7, 1.4) - p),
 			normalize(vec3(-2.4, .5, 1.3) - p),
@@ -71,38 +54,16 @@ void main(void) {
 		}
 		color *= dot(l, vec4(.7, .8, .9, .5));
 	}
-	color += /*cubicPulse(.5, .5, fract(3. * time / 1000.)) */ a * glow_color;
 
-	gl_FragColor = vec4(color, 1.);
-
+	out_color = vec4(color, 1.);
 }
 
 vec2 f(vec3 p) {
 	p.x -= 1.;
-	vec2 sphere1 = vec2(length(p) - .7, 0.);
+	vec2 sphere1 = vec2(sphere(p, .7), 0.);
 	p.x += 2.;
-	vec2 sphere2 = vec2(length(p) - .7, 1.);
+	vec2 sphere2 = vec2(sphere(p, .7), 1.);
 	vec2 bottom = vec2(p.y + 2., 2.);
-	vec2 bounding = vec2(50. - length(p - viewPosition), 3.);
+	vec2 bounding = vec2(-sphere(p - view_position, 50.), 3.);
 	return min_material(min_material(sphere1, sphere2), min_material(bottom, bounding));
-}
-
-vec3 calcNormal(vec3 p) {
-	vec2 epilepsilon = vec2(.001, 0.);
-
-	return normalize(vec3(
-				f(p + epilepsilon.xyy).x - f(p - epilepsilon.xyy).x,
-				f(p + epilepsilon.yxy).x - f(p - epilepsilon.yxy).x,
-				f(p + epilepsilon.yyx).x - f(p - epilepsilon.yyx).x
-			     ));
-}
-
-float cubicPulse( float c, float w, float x ) {
-    x = abs(x - c);
-    x /= w;
-	return 1. - smoothstep(0., 1., x);
-}
-
-vec2 min_material(vec2 a, vec2 b) {
-	return mix(a, b, float(a.x > b.x));
 }
--- blinkizeuch™/vertex.h
+++ blinkizeuch™/vertex.h
@@ -2,11 +2,10 @@
 #define VERTEX_H
 
 const char vertex_source[] = "\
-attribute vec2 coord2d;\
-varying vec2 pos;\
+#version 330\n\
+in vec2 coord2d;\
 \
 void main(void) {\
-	pos = coord2d;\
 	gl_Position = vec4(coord2d, 0.0, 1.0);\
 }\
 ";
