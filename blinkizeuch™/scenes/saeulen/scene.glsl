2de5ab63a1a0464e1d227b97d705cfa178ca4663
Vincent Schüßler <vincent@vschuessler.org>
2013-09-29 16:06:07 +0200

    Port sauelen to libblink

--- blinkizeuch™/scenes/saeulen/scene.glsl
+++ blinkizeuch™/scenes/saeulen/scene.glsl
@@ -1,29 +1,13 @@
-#version 120
-
 void initValues();
-float f(vec3 p);
-float sphere(vec3 p, float r);
+vec2 f(vec3 p);
 float spikeball(vec3 p);
-float sdBox(vec3 p, vec3 b);
 float octoBox(vec3 p, float l, float h);
-mat3 rX(float theta);
-mat3 rY(float theta);
-mat3 rZ(float theta);
-vec3 calcNormal(vec3 p);
 float ao(vec3 p, vec3 n, float d, float i);
 vec3 lighting(vec3 p, vec3 color, vec3 direction, vec3 normal, out vec3 light_color);
-vec3 marching(vec3 p, vec3 direction, out int i);
 float softshadow(vec3 ro, vec3 rd, float k);
-vec3 hsv2rgb(vec3 c);
-vec3 rgb2hsv(vec3 c);
 
-uniform float aspect;
-uniform vec2 res;
-uniform float time;
-
-uniform vec3 viewPosition;
-uniform vec3 viewDirection;
-uniform vec3 viewUp;
+DEFINE_MARCH(march, f)
+DEFINE_NORMAL(calc_normal, f)
 
 uniform float light1_x;
 uniform float light1_y;
@@ -41,8 +25,6 @@ vec3 color_fog = vec3(.67, .1, .8); // in hsv
 vec3 color_saeulen = vec3(1., 1., 1.);
 vec3 color_lights[number_lights];
 
-mat3 rotation_spikeballs;
-
 float intensity_lights[number_lights];
 
 vec3 c[19];
@@ -50,27 +32,22 @@ vec3 c[19];
 void main() {
 	initValues();
 
-	vec3 viewRight = cross(viewDirection, viewUp);
-	mat3 viewCamera = transpose(mat3(viewRight, viewUp, -viewDirection));
-
-	vec3 camera = viewPosition;
-	vec3 direction = normalize(vec3((gl_FragCoord.xy - .5 * res) / res.y, -1.)) * viewCamera;
-
 	vec3 color = vec3(0.);
-	vec3 p = camera;
+	vec3 p = view_position;
+	vec3 direction = get_direction();
 	vec3 light_color_factor = vec3(1.);
 	float reflection_factor = 1.;
 
 	for (int reflections = 0; reflections < 1; reflections++) {
 		int i;
-		p = marching(p, direction, i);
+		p = march(p, direction, i);
 		if(i < 100) {
-			vec3 normal = calcNormal(p);
+			vec3 normal = calc_normal(p);
 			vec3 light_color;
 			vec3 newColor = lighting(p, color_saeulen, direction, normal, light_color);
-			//newColor *= 2. - exp( -2. * pow(distance(p, camera) / 20., 7.)); // fog
+			//newColor *= 2. - exp( -2. * pow(distance(p, view_position) / 20., 7.)); // fog
 			vec3 hsv_newColor = rgb2hsv(newColor);
-			newColor = hsv2rgb(mix(hsv_newColor, vec3(hsv_newColor.x, color_fog.yz), smoothstep(2., 7., distance(p, camera)))); // fog
+			newColor = hsv2rgb(mix(hsv_newColor, vec3(hsv_newColor.x, color_fog.yz), smoothstep(2., 7., distance(p, view_position)))); // fog
 			//newColor += float(i) / 100.; // iteration glow
 			color += newColor * reflection_factor * light_color_factor;
 			light_color_factor *= light_color;
@@ -82,7 +59,7 @@ void main() {
 		}
 	}
 	color += vec3(.8, .8, .9) * smoothstep(0., res.y, gl_FragCoord.y);
-	gl_FragColor = vec4(color, 1.);
+	out_color = vec4(color, 1.);
 }
 
 void initValues() {
@@ -96,15 +73,10 @@ void initValues() {
 
 	intensity_lights[0] = .7;
 	intensity_lights[1] = .7;
-
-	rotation_spikeballs =
-		rZ(radians(time/1000. * 15)) *
-		rY(radians(time/1000. * 8)) *
-		rX(radians(time/1000. * 10));
 }
 
-float f(vec3 p) {
-	float sphery = -sphere(p - viewPosition, 50.); // bounding sphere
+vec2 f(vec3 p) {
+	float sphery = -sphere(p - view_position, 50.); // bounding sphere
 
 	float floor_plane = p.y+.5;
 	float ceiling_plane = -p.y+15.;
@@ -123,17 +95,7 @@ float f(vec3 p) {
 	saeulen -= fugen;
 
 	//vec3 b = p;
-	return min(min(sphery,saeulen), room);
-}
-
-float sphere(vec3 p, float r) {
-	return length(p) - r;
-}
-
-float sdBox(vec3 p, vec3 b) {
-	vec3 d = abs(p) - b;
-	return min(max(d.x, max(d.y, d.z)), 0.0) +
-		length(max(d,0.0));
+	return vec2(min(min(sphery,saeulen), room), 0.);
 }
 
 float octoBox(vec3 p, float l, float h){
@@ -142,47 +104,12 @@ float octoBox(vec3 p, float l, float h){
 	vec3 b3 = rY(radians(135.)) * p;
 	vec3 b4 = rY(radians(180.)) * p;
 	vec3 viertel = vec3(l, h, l+2*sqrt((l*l)/2));
-	return min(min(sdBox(b1, viertel),sdBox(b4, viertel)), min(sdBox(b2, viertel),sdBox(b3, viertel)));
+	return min(min(box(b1, viertel),box(b4, viertel)), min(box(b2, viertel),box(b3, viertel)));
 }
-
-mat3 rX(float theta) {
-	return mat3(
-		1., 0., 0.,
-		0., cos(theta), sin(theta),
-		0., -sin(theta), cos(theta)
-	);
-}
-
-mat3 rY(float theta) {
-	return mat3(
-		cos(theta), 0., -sin(theta),
-		0., 1., 0.,
-		sin(theta), 0., cos(theta)
-	);
-}
-
-mat3 rZ(float theta) {
-	return mat3(
-		cos(theta), sin(theta), 0.,
-		-sin(theta), cos(theta), 0.,
-		0., 0., 1.
-	);
-}
-
-vec3 calcNormal(vec3 p) {
-	vec2 epilepsilon = vec2(.001, 0.);
-
-	return normalize(vec3(
-				f(p + epilepsilon.xyy) - f(p - epilepsilon.xyy),
-				f(p + epilepsilon.yxy) - f(p - epilepsilon.yxy),
-				f(p + epilepsilon.yyx) - f(p - epilepsilon.yyx)
-			     ));
-}
-
 float ao(vec3 p, vec3 n, float d, float i) { // ambient occlusion ans sub surface scattering
 	float o, s = sign(d);
 	for (o=s*.5+.5;i>0.;i--) {
-		o-=(i*d-f(p+n*i*d*s))/exp2(i);
+		o-=(i*d-f(p+n*i*d*s)[0])/exp2(i);
 	}
 	return o;
 }
@@ -203,43 +130,15 @@ vec3 lighting(vec3 p, vec3 color, vec3 direction, vec3 normal, out vec3 light_co
 	return color * light_color;
 }
 
-vec3 marching(vec3 p, vec3 direction, out int i) {
-	float walked = 0.;
-	for (i=0; i < 100; i++) {
-		float dist = f(p);
-		p += direction * dist;
-		dist = abs(dist);
-		walked += dist;
-
-		if (dist < .001 * walked) break;
-	}
-	return p;
-}
-
 float softshadow(vec3 ro, vec3 rd, float k ) {
 	float res = 1.0;
 	float t = 0.01;
 	for(int i=0; i < 64; i++)
 	{
-		float h = f(ro + rd*t);
+		float h = f(ro + rd*t)[0];
 		res = min( res, max(k*h/t, 0.) );
 		t += clamp(h, 0.02, 0.1);
 	}
 	return clamp(res, 0., 1.);
 }
 
-vec3 rgb2hsv(vec3 c) {
-    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
-
-    float d = q.x - min(q.w, q.y);
-    float e = 1.0e-10;
-    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
-}
-
-vec3 hsv2rgb(vec3 c) {
-    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-}
