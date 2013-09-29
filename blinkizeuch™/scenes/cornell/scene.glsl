8ae2eaf83bd85cdcc58951da9ff944c3389c5ecb
Vincent Schüßler <vincent@vschuessler.org>
2013-09-29 16:13:49 +0200

    Port cornell to libblink

--- blinkizeuch™/scenes/cornell/scene.glsl
+++ blinkizeuch™/scenes/cornell/scene.glsl
@@ -1,18 +1,7 @@
-#version 120
-
-uniform vec3 viewPosition;
-uniform vec3 viewUp;
-uniform vec3 viewDirection;
-uniform vec2 res;
-uniform float time;
-
 vec2 f(vec3 p);
-vec3 calcNormal(vec3 p);
-float sdBox(vec3 p, vec3 b);
-mat3 rX(float theta);
-mat3 rY(float theta);
-mat3 rZ(float theta);
-vec4 sin_cos_vec(float theta);
+
+DEFINE_MARCH(march, f)
+DEFINE_NORMAL(calc_normal, f)
 
 vec3 colors[4];
 
@@ -20,10 +9,6 @@ mat3 rot_box1;
 mat3 rot_box2;
 
 void main(void) {
-	vec3 viewRight = cross(viewDirection, viewUp);
-	mat3 camera = mat3(viewRight, viewUp, -viewDirection);
-	vec3 direction = camera * normalize(vec3((gl_FragCoord.xy - .5 * res) / res.y , -1.));
-
 	colors[0] = vec3(1.);
 	colors[1] = vec3(1., 0., 0.);
 	colors[2] = vec3(0., 1., 0.);
@@ -32,22 +17,14 @@ void main(void) {
 	rot_box1 = rY(radians(20.));
 	rot_box2 = rY(radians(-20.));
 
-	vec3 p = viewPosition;
-	int i = 0;
-	float walked = 0.;
-	for(i = 0; i < 100; i++) {
-		float dist = f(p).x;
-		p += direction * dist;
-
-		dist = abs(dist);
-		walked += dist;
-		if(dist < .001 * walked) break;
-	}
+	vec3 direction = get_direction();
+	int i;
+	vec3 p = march(view_position, direction, i);
 
 	vec3 color = vec3(0.);
 	if(i < 100) {
 		int material = int(f(p).y);
-		vec3 normal = calcNormal(p);
+		vec3 normal = calc_normal(p);
 		vec4 l = max(normal * mat4x3(
 			normalize(vec3( .25, 3.5,  .25) - p),
 			normalize(vec3(-.25, 3.5,  .25) - p),
@@ -58,8 +35,7 @@ void main(void) {
 		color = colors[material] * dot(l, vec4(.25));
 	}
 
-	gl_FragColor = vec4(color, 1.);
-
+	out_color = vec4(color, 1.);
 }
 
 vec2 f(vec3 p) {
@@ -69,57 +45,12 @@ vec2 f(vec3 p) {
 	float top = -p.y + room_size;
 	float white_walls = min(back, min(bottom, top));
 	float box_size = .8;
-	float box1 = sdBox(rot_box1 * p - vec3(-box_size * 1.5, -3. + box_size * 2, -box_size), vec3(box_size, box_size * 2., box_size));
-	float box2 = sdBox(rot_box2 * p - vec3( box_size *1.5, -3. + box_size,  box_size), vec3(box_size));
-	float white_objects = min(white_walls, min(box1, box2));
-	float red_wall = p.x + room_size;
-	float green_wall = -p.x + room_size;
-	float light = sdBox(p - vec3(0., room_size, 0.), vec3(.5, .001, .5));
-	float dist = min(min(white_objects, red_wall), min(green_wall, light));
-	float material = dot(step(vec4(white_objects, red_wall, green_wall, light), vec4(dist)), vec4(0., 1., 2., 3.));
-	return vec2(dist, material);
+	float box1 = box(rot_box1 * p - vec3(-box_size * 1.5, -3. + box_size * 2, -box_size), vec3(box_size, box_size * 2., box_size));
+	float box2 = box(rot_box2 * p - vec3( box_size *1.5, -3. + box_size,  box_size), vec3(box_size));
+	vec2 white_objects = vec2(min(white_walls, min(box1, box2)), 0.);
+	vec2 red_wall = vec2(p.x + room_size, 1.);
+	vec2 green_wall = vec2(-p.x + room_size, 2.);
+	vec2 light = vec2(box(p - vec3(0., room_size, 0.), vec3(.5, .001, .5)), 3.);
+	return min_material(min_material(white_objects, red_wall), min_material(green_wall, light));
 }
 
-vec3 calcNormal(vec3 p) {
-	vec2 epilepsilon = vec2(.001, 0.);
-
-	return normalize(vec3(
-				f(p + epilepsilon.xyy).x - f(p - epilepsilon.xyy).x,
-				f(p + epilepsilon.yxy).x - f(p - epilepsilon.yxy).x,
-				f(p + epilepsilon.yyx).x - f(p - epilepsilon.yyx).x
-			     ));
-}
-
-float sdBox(vec3 p, vec3 b ) {
-  vec3 d = abs(p) - b;
-  return min(max(d.x,max(d.y,d.z)),0.0) +
-         length(max(d,0.0));
-}
-
-mat3 rX(float theta) {
-	vec4 a = sin_cos_vec(theta);
-	vec3 b = vec3(1., 1., -1.);
-	return mat3(
-		a.xyy, a.ywz * b, a.yzw
-	);
-}
-
-mat3 rY(float theta) {
-	vec4 a = sin_cos_vec(theta);
-	vec3 b = vec3(-1., 1., 1.);
-	return mat3(
-		a.wyz, a.yxy, a.zyw * b
-	);
-}
-
-mat3 rZ(float theta) {
-	vec4 a = sin_cos_vec(theta);
-	vec3 b = vec3(-1., 1., 1.);
-	return mat3(
-		a.wzy * b, a.zwy * b, a.yyx
-	);
-}
-
-vec4 sin_cos_vec(float theta) {
-	return vec4(1., 0., sin(theta), cos(theta));
-}
