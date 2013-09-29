422f3765572272cb6bbf4407db6d992bcfb88d73
Vincent Schüßler <vincent@vschuessler.org>
2013-09-29 11:03:10 +0200

    Change directory structure of scenes

--- blinkizeuch™/config.h
+++ blinkizeuch™/config.h
@@ -5,4 +5,7 @@ int height = 600;
 
 const char window_caption[] = "blinkizeuch™";
 
+const char fragment_name[] = "scene.glsl";
+const char config_name[] = "config.json";
+
 #endif
--- blinkizeuch™/shader/fragment.glsl
+++
@@ -1,7 +0,0 @@
-varying vec2 pos;
-uniform float aspect;
-
-void main(void) {
-	vec3 color = vec3(1.0, 1.0, 0.0);
-	gl_FragColor = vec4(color, 1.0);
-}
