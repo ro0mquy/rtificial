8ba6689b0bfd69ad29f4b47a1b6ec190331d3202
Vincent Schüßler <vincent@vschuessler.org>
2014-01-03 00:59:47 +0100

    Add windows with reflections

--- blinkizeuch™/scenes/hochhinhaus/scene.glsl
+++ blinkizeuch™/scenes/hochhinhaus/scene.glsl
@@ -8,50 +8,92 @@ DEFINE_AO(ao, f)
 const int MAT_UNTENRUM = 1;
 const int MAT_HOUSES = 2;
 const int MAT_HOUSE2 = 3;
+const int MAT_WINDOWS = 4;
 
 uniform vec3 color_untenrum;
 uniform vec3 color_house1;
 uniform vec3 color_house2;
 uniform float noisiness;
 uniform float corner_width;
+uniform float ambient_intensity;
+uniform vec3 color_windows;
+uniform float reflectivity;
+
+uniform vec3 color_foo1;
+uniform vec3 color_foo2;
+uniform float foo1;
+uniform float foo2;
 
 void main(void) {
 	vec3 dir = get_direction();
-	int i;
-	vec3 hit = march(view_position, dir, i);
-	vec3 color = vec3(0);
-	if(i < 100) {
-		vec3 normal = calc_normal(hit);
-		vec3 light = vec3(20, 20, 20);
-		int material = int(f(hit)[1]);
-		float factor = 1;
-		if(material == 0) {
-			out_color = vec4(vec3(0), 1);
-			return;
-		} else if(material == MAT_UNTENRUM) {
-			color = color_untenrum;
-			float noise = smooth_noise(noisiness * hit);
-			color += noise * .3;
-		} else if(material == MAT_HOUSES) {
-			color = color_house1;
-		} else if(material == MAT_HOUSE2) {
-			color = color_house2;
+	vec3 final_color = vec3(0);
+	int max_bounces = 2;
+	for(int i = 0; i < max_bounces; i++) {
+		int i;
+		vec3 hit = march(view_position, dir, i);
+		if(i < 100) {
+			vec3 normal = calc_normal(hit);
+			vec3 light = vec3(20, 20, 20);
+			int material = int(f(hit)[1]);
+			float factor = 1;
+			vec3 color;
+			bool cont = false;
+			if(material == 0) {
+				 color = vec3(0);
+			} else if(material == MAT_UNTENRUM) {
+				color = color_untenrum;
+				float noise = smooth_noise(noisiness * hit);
+				color += noise * .3;
+			} else if(material == MAT_HOUSES) {
+				color = color_house1;
+			} else if(material == MAT_HOUSE2) {
+				color = color_house2;
+			} else if(material == MAT_WINDOWS) {
+				color = color_windows;
+				dir = reflect(dir, normal);
+				factor *= reflectivity;
+				cont = true;
+			}
+			float local_factor = factor;
+			local_factor *= max(dot(normal, normalize(light - hit)), 0);
+			local_factor += ambient_intensity * ao(hit, normal, .15, 5);
+			color *= local_factor;
+			factor *= local_factor;
+			final_color += color;
+			if(!cont) break;
+		} else {
+			break;
 		}
-		factor *= max(dot(normal, normalize(light - hit)), 0);
-		factor += 0.2 * ao(hit, normal, .15, 5);
-		color *= factor;
 	}
-	out_color = vec4(color, 1);
+	out_color = vec4(final_color, 1);
 }
 
-float haus_mit_ecken(vec3 p, vec3 box_dim, float corner_width) {
+vec2 haus_mit_ecken(vec3 p, vec3 box_dim, float corner_width, int material) {
 	float house = box(p, box_dim);
 	for(int x = 0; x < 2; x++) {
 		for(int z = 0; z < 2; z++) {
 			house = max(house, -box(trans(p, (2 * x - 1) * box_dim.x, 0, (2 * z - 1) * box_dim.z), vec3(corner_width, box_dim.y * 1.1, corner_width)));
 		}
 	}
-	return house;
+	vec2 window_size = vec2(.4, .8);
+	vec3 p2 = p;
+	p2.z = abs(p.z);
+	vec3 q = trans(p2, 0, 0, box_dim.x);
+	vec3 q2 = domrep(q, window_size.x * 2. + .1, window_size.y * 2. + .1, 1.);
+	q2.z = q.z;
+	float windows_z = box(q2, vec3(window_size, .001));
+	windows_z = max(windows_z, box(q, vec3(box_dim.x - corner_width, box_dim.y, 1.)));
+
+	p2 = p;
+	p2.x = abs(p.x);
+	q = trans(p2, box_dim.z, 0, 0);
+	q2 = domrep(q, 1., window_size.y * 2. + .1, window_size.x * 2. + .1);
+	q2.x = q.x;
+	float windows_x = box(q2, vec3(.001, window_size.y, window_size.x));
+	windows_x = max(windows_x, box(q, vec3(1., box_dim.y, box_dim.z - corner_width)));
+
+	float all_windows = min(windows_z, windows_x);
+	return min_material(vec2(house, material), vec2(all_windows, MAT_WINDOWS));
 }
 
 vec2 f(vec3 p) {
@@ -64,9 +106,9 @@ vec2 f(vec3 p) {
 	float house1_height = 40. / 2.;
 	float house2_height = 30. / 2.;
 	vec2 untenrum = vec2(plane(trans(p, 0, 0, 0), vec3(0, 1, 0)), MAT_UNTENRUM);
-	vec2 house1 = vec2(haus_mit_ecken(trans(p, 0, 2 * house2_height + house1_height, 0), vec3(house1_width, house1_height, house1_width), corner_width * house1_width), MAT_HOUSES);
-	vec2 house2 = vec2(haus_mit_ecken(trans(p, 0, house2_height, 0), vec3(house2_width, house2_height, house2_width),  corner_width * house2_width), MAT_HOUSE2);
-	vec2 house3 = vec2(haus_mit_ecken(trans(p, 0, 2 * (house2_height + house1_height) + house3_width, 0), vec3(house3_width, house3_height, house3_width),  corner_width * house3_width), MAT_HOUSE2);
+	vec2 house1 = haus_mit_ecken(trans(p, 0, 2 * house2_height + house1_height, 0), vec3(house1_width, house1_height, house1_width), corner_width * house1_width, MAT_HOUSES);
+	vec2 house2 = haus_mit_ecken(trans(p, 0, house2_height, 0), vec3(house2_width, house2_height, house2_width),  corner_width * house2_width, MAT_HOUSE2);
+	vec2 house3 = haus_mit_ecken(trans(p, 0, 2 * (house2_height + house1_height) + house3_width, 0), vec3(house3_width, house3_height, house3_width),  corner_width * house3_width, MAT_HOUSE2);
 	vec2 house = min_material(house1, min_material(house2, house3));
 	float houses_height = 6;
 	vec3 q = trans(p, 0, houses_height, 0);
