d095dd047971799df13df8e9020c18399491699a
ro0mquy <git@ro0mquy.de>
2014-01-04 00:40:02 +0100

    fixed warnings

--- blinkizeuch™/flight.h
+++ blinkizeuch™/flight.h
@@ -15,7 +15,7 @@ typedef struct flight_t {
 	int duration;
 } flight_t;
 
-flight_t flight_new(const camera_t* p0, const camera_t* p1, const camera_t* p2, const camera_t* p3, const int start_time, const int duration);
+flight_t flight_new(camera_t* p0, camera_t* p1, camera_t* p2, camera_t* p3, const int start_time, const int duration);
 
 camera_t flight_get_camera(const flight_t* const flight, const int time);
 
