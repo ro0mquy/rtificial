cf96e551bcfae0ca46992d8bcdef579802b030b3
Vincent Schüßler <vincent@vschuessler.org>
2014-01-03 17:20:14 +0100

    Added project creation script with boilerplate templates.

---
+++ blinkizeuch™/create_project.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+mkdir -p scenes/$1
+cp scenes/template/scene.glsl scenes/$1/
+cp scenes/template/config.json scenes/$1/
---
+++ blinkizeuch™/scenes/template/scene.glsl
@@ -0,0 +1,26 @@
+vec2 f(vec3);
+
+DEFINE_MARCH(march, f)
+DEFINE_NORMAL(calc_normal, f)
+DEFINE_AO(ao, f)
+
+uniform vec3 color_foo1;
+uniform vec3 color_foo2;
+uniform float foo1;
+uniform float foo2;
+
+void main(void){
+	vec3 dir = get_direction();
+	vec3 final_color = vec3(0);
+	int i;
+	vec3 hit = march(view_position, dir, i);
+	if(i < 100){
+		vec3 normal = calc_normal(hit);
+		final_color = vec3(1.0);
+	}
+	out_color = vec4(final_color, 1); 
+} 
+
+vec2 f(vec3 p){
+	return vec2(sphere(p, 4.),1);
+}
