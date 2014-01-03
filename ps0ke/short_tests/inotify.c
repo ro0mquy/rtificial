a8c4d01ac3c497fce50c2f9260f444974ff0a3d5
Ps0ke <mail@ps0ke.de>
2014-01-03 15:17:59 +0100

    Made inotify non-blocking, still not working though. fuck this shit

--- ps0ke/short_tests/inotify.c
+++ ps0ke/short_tests/inotify.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/inotify.h>
+#include <sys/fcntl.h>
 
 #define EVENT_SIZE  ( sizeof (struct inotify_event) )
 #define BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
@@ -13,20 +14,24 @@ int main( int argc, char **argv ){
 
 	fd = inotify_init();
 	if (fd < 0)
-		puts("error: inotify_init");
+		puts("error: inotify_init()");
+	if(fcntl(fd, F_SETFL, O_NONBLOCK))
+		puts("error: fcntl()");
 
 	wd = inotify_add_watch( fd, "/tmp/foo", IN_MODIFY);
 
-	while(23){
+	int cont = 23;
+	while(cont){
 		length = read(fd, buffer, BUF_LEN);
-		if (length < 0)
-			puts("error: read");
 
 		i = 0;
 		while (i < length){
 			struct inotify_event *event = (struct inotify_event*) &buffer[i];
-			if (event->mask & IN_MODIFY)
+			printf("i: %i; length: %i; mask: %i; IN_MODIFY: %i, mask & flag: %i\n", i, length, event->mask, IN_MODIFY, event->mask & IN_MODIFY);
+			if (event->mask & IN_MODIFY){
 				puts("The file was modified.");
+				cont = 0;
+			}
 
 			i += EVENT_SIZE + event->len;
 		}
