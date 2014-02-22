74af0ff9e8e92f87ddd85ad5e546c0fdbaaf431f
ro0mquy <git@ro0mquy.de>
2014-02-22 19:29:24 +0100

    added very simple sharpen/kernel support

--- blinkizeuch™/scenes/camera_test/post.glsl
+++ blinkizeuch™/scenes/camera_test/post.glsl
@@ -4,17 +4,59 @@ in vec2 texcoord;
 out vec4 outColor;
 uniform sampler2D tex;
 
+/*
+float w_pixel = 1. / textureDimension(tex).x;
+float h_pixel = 1. / textureDimension(tex).y;
+
+vec2 offset[9] = vec2[](
+	vec2(-w_pixel, -h_pixel),
+	vec2(-w_pixel, 0.),
+	vec2(-w_pixel, +h_pixel),
+	vec2(0., -h_pixel),
+	vec2(0., 0.),
+	vec2(0., +h_pixel),
+	vec2(+w_pixel, -h_pixel),
+	vec2(+w_pixel, 0.),
+	vec2(+w_pixel, +h_pixel)
+);
+// */
+
+ivec2 offset[9] = ivec2[](
+	ivec2(-1, -1),
+	ivec2(-1, 0),
+	ivec2(-1, -1),
+	ivec2(0, 1),
+	ivec2(0, 0),
+	ivec2(0, 1),
+	ivec2(1, -1),
+	ivec2(1, 0),
+	ivec2(1, 1)
+);
+
+float kernel[9] = float[](
+	0., -1., 0.,
+	-1., 5., -1.,
+	0., -1., 0.
+);
+
+vec3 sharpen() {
+	vec3 sum = vec3(0.);
+
+	for (int i = 0; i < 9; i++) {
+		vec3 color = textureOffset(tex, texcoord, offset[i]).rgb;
+		sum += color * kernel[i];
+	}
+
+	return sum;
+}
+
 void main() {
-	vec3 color = texture(tex, texcoord).xyz;
+	vec3 color = texture(tex, texcoord).rgb;
 
 	if (texcoord.x > .5) {
-		color = vec3(1.0, 1.0, 1.0) - texture(tex, texcoord).xyz;
+		color = sharpen();
 	}
 
-	if (texcoord.y < .5) {
-		float avg = (color.r + color.g + color.b) / 3.0;
-		color = vec3(avg);
-	}
 
 	outColor = vec4(color, 1.);
 }
