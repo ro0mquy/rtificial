1259e59513fe913a11e1e8fe3b7aa4967ee67681
ro0mquy <git@ro0mquy.de>
2014-02-26 02:02:57 +0100

    save depth to texture and make available in postprocessing

--- blinkizeuch™/scenes/camera_test/scene.glsl
+++ blinkizeuch™/scenes/camera_test/scene.glsl
@@ -55,7 +55,10 @@ void main(void) {
 		color *= dot(l, vec4(.7, .8, .9, .5));
 	}
 
+	float depth = distance(view_position, p);
+
 	out_color = vec4(color, 1.);
+	out_depth = vec4(depth, 0., 0., 1.);
 }
 
 vec2 f(vec3 p) {
