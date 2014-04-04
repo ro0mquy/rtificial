87949b4c4fdc98047724e089713af038f04dfbed
Vincent Schüßler <vincent@vschuessler.org>
2014-04-04 11:22:23 +0200

    Add first iteration of pythagorean tree

---
+++ blinkizeuch™/scenes/pythagorean/scene.glsl
@@ -0,0 +1,58 @@
+vec2 f(vec3);
+
+DEFINE_MARCH(march, f)
+DEFINE_NORMAL(calc_normal, f)
+DEFINE_AO(ao, f)
+
+uniform vec3 color_foo1;
+uniform vec3 color_foo2;
+uniform float foo1;
+uniform float foo2;
+
+void main(void){
+	vec3 dir = get_direction();
+	vec3 final_color = vec3(0);
+	int i;
+	vec3 hit = march(view_position, dir, i);
+	if(i < 100){
+		int material = int(f(hit)[1]);
+		if(material == 1) {
+			vec3 normal = calc_normal(hit);
+			vec3 to_light = vec3(10) - hit;
+			final_color = .8 * vec3(1) * lambert(to_light, normal);
+			final_color += .1;
+			final_color *= ao(hit, normal, .15, 5.);
+		}
+	}
+	out_color = pow(final_color, vec3(1./2.2));
+}
+
+float cube(vec3 p) {
+	return box(p, vec3(1.));
+}
+
+vec2 f(vec3 p){
+	float alpha = foo1 * 90.;
+	float beta = radians(90. - alpha);
+	alpha = radians(alpha);
+	float a = cos(alpha);
+	float b = sqrt(1. - a * a);
+	float tree = cube(p);
+
+	float q_a = a * 1.;
+	vec3 q = trans(p, 0, 1 + q_a, -(1. - q_a));
+	q = trans(q, 0, -q_a, -q_a);
+	q = rX(alpha) * q;
+	q = trans(q, 0, q_a, q_a);
+	tree = min(tree, scale(cube, q, q_a));
+
+	float r_a = b * 1.;
+	vec3 r = trans(p, 0, 1 + r_a, 1. - r_a);
+	r = trans(r, 0, -r_a, r_a);
+	r = rX(-beta) * r;
+	r = trans(r, 0, r_a, -r_a);
+	tree = min(tree, scale(cube, r, r_a));
+
+	vec2 bounding = vec2(-sphere(p - view_position, 1000.), 2);
+	return min_material(vec2(tree,1), bounding);
+}
