1d18e49636775b31ea04b7bdc995a3bc3e98d76d
ro0mquy <git@ro0mquy.de>
2014-04-08 22:29:44 +0200

    add a trichter to the gitternetz

--- blinkizeuch™/scenes/gitternetz/scene.glsl
+++ blinkizeuch™/scenes/gitternetz/scene.glsl
@@ -15,6 +15,7 @@ uniform vec3 color_kugel;
 uniform float radius_kugel;
 uniform float dy_kugel;
 uniform float sigma_delle;
+uniform float sigma_trichter;
 
 #define mat_bounding 0
 #define mat_gitter 1
@@ -53,27 +54,34 @@ void main(void) {
 vec2 f(vec3 p) {
 	vec2 bounding = vec2(-sphere(p, 50.), mat_bounding);
 
-	vec3 p_delle = trans(p, 5 * sin(TAU / 4 * time), 0, 3 * sin(TAU / 5 * time));
+	vec2 pos_delle = vec2(5 * sin(TAU / 4 * time), 3 * sin(TAU / 5 * time));
+	vec3 p_delle = trans(p, pos_delle.x, 0, pos_delle.y);
 
 	float l = length(p_delle.xz) / sigma_delle;
 	float f_delle = 1 / (l*l + 1);
-	//float f_delle = exp(-.5 * l*l);
 	f_delle *= dy_kugel;
 
+	vec2 pos_trichter = vec2(25, 0);
+	vec3 p_trichter = trans(p, pos_trichter.x, 0, pos_trichter.y);
+	float d = length(p_trichter.xz);
+	float f_trichter = - sigma_trichter / d;
+
 	vec3 p_untergrund = domrep(p, 5, 5, 5);
-	p_untergrund.y -= f_delle / sqrt(-p.y);
+	p_untergrund.y -= (f_delle + f_trichter) / sqrt(-p.y);
 	float f_untergrund = roundbox(p_untergrund, vec3(.04), .02);
-	//float f_untergrund = sphere(p_untergrund, .05);
 	f_untergrund = max(f_untergrund, p.y);
 	vec2 vec_untergrund = vec2(f_untergrund, mat_untergrund);
 
 	vec3 p_gitter = domrep(p, 1, 1, 1);
-	p_gitter.y = p.y - f_delle;
 	p_gitter = abs(p_gitter);
-	float f_gitter = smax(p_gitter.y, smin(p_gitter.x, p_gitter.z, .01), .01) - .01;
+	p_gitter.y = p.y - f_delle;
+
+	float de_trichter = abs(f_trichter - p_gitter.y) / sqrt(1 + f_trichter * f_trichter / d / d);
+	float f_gitter = smax(de_trichter, smin(p_gitter.x, p_gitter.z, .01), .01) - .01;
 	vec2 vec_gitter = vec2(f_gitter, mat_gitter);
 
-	vec3 p_kugel = trans(p_delle, 0, dy_kugel + radius_kugel, 0);
+	float f_trichter_pos_kugel = - sigma_trichter / distance(pos_delle, pos_trichter);
+	vec3 p_kugel = trans(p_delle, 0, f_trichter_pos_kugel + dy_kugel + radius_kugel, 0);
 	float f_kugel = sphere(p_kugel, radius_kugel);
 	vec2 vec_kugel = vec2(f_kugel, mat_kugel);
 
