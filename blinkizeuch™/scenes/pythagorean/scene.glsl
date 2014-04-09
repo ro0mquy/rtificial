ea28ba705514b712a073ba3c37bf3ac48a7976ad
Vincent Schüßler <vincent@vschuessler.org>
2014-04-09 14:10:05 +0200

    Improve floor and performance of pythagorean

--- blinkizeuch™/scenes/pythagorean/scene.glsl
+++ blinkizeuch™/scenes/pythagorean/scene.glsl
@@ -1,7 +1,9 @@
 vec2 f(vec3);
+vec2 g(vec3);
 
 DEFINE_MARCH(march, f)
 DEFINE_NORMAL(calc_normal, f)
+DEFINE_NORMAL(calc_normal_floor, g)
 DEFINE_AO(ao, f)
 
 uniform vec3 color_foo1;
@@ -16,13 +18,15 @@ void main(void){
 	vec3 hit = march(view_position, dir, i);
 	if(i < 100){
 		int material = int(f(hit)[1]);
-		vec3 normal = calc_normal(hit);
 		vec3 light = vec3(0, 10, 0);
+		vec3 normal;
 		if(material == 1) {
+			normal =  calc_normal(hit);
 			vec3 to_light = light - hit;
 			final_color = .9 * vec3(oren_nayar(to_light, normal, -dir, foo2));
 			final_color += .1;
 		} else if(material == 2) {
+			normal = calc_normal_floor(hit);
 			final_color = vec3(1.);
 		}
 		final_color *= ao(hit, normal, .2, 10.);
@@ -66,6 +70,23 @@ float pythagoraen(vec3 p) {
 	}
 	return tree;
 }
+
+float fbm(vec2 p) {
+	float sum = 0.;
+	float amplitude = .5;
+	float freq = 1.;
+	for(int i = 0; i < 3; i++) {
+		sum += classic_noise(p * freq) * amplitude;
+		freq *= 2.;
+		amplitude *= .5;
+	}
+	return sum;
+}
+
+float f_floor(vec3 p) {
+	return p.y + 1.;
+}
+
 vec2 f(vec3 p) {
 	float c = 20;
 	vec3 q = domrep(p, c, 1., c);
@@ -73,6 +94,9 @@ vec2 f(vec3 p) {
 	float foo = 4. * rand(200. * floor(p.xz / c) - 123.);
 	float tree = pythagoraen(rY(foo * radians(90.)) * q);
 	vec2 bounding = vec2(-sphere(transv(p, view_position), 200.), 2);
-	return min_material(vec2(tree,1), min_material(vec2(p.y + 1. + classic_noise(p.xz * 3.) * .15, 2.), bounding));
+	return min_material(vec2(tree,1), min_material(vec2(f_floor(p), 2.), bounding));
 }
 
+vec2 g(vec3 p) {
+	return vec2(f_floor(p) + fbm(p.xz * 5.) * .15, 2.);
+}
