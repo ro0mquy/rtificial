bdb6ebbb3e4cf1337091e702321be8bf896c8783
Vincent Schüßler <vincent@vschuessler.org>
2014-04-14 12:27:46 +0200

    Get rid of additional vertex shader

--- blinkizeuch™/shader_sources.h
+++ blinkizeuch™/shader_sources.h
@@ -4,15 +4,6 @@
 static const char vertex_source[] = "\
 #version 330\n\
 in vec2 coord2d;\
-\
-void main(void) {\
-	gl_Position = vec4(coord2d, 0.0, 1.0);\
-}\
-";
-
-static const char post_vertex_source[] = "\
-#version 330\n\
-in vec2 coord2d;\
 out vec2 texcoord;\
 void main() {\
 	texcoord = (coord2d + 1.) / 2.;\
