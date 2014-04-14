4c05431464a359ea22756ab606de82800a9aed3d
Vincent Schüßler <vincent@vschuessler.org>
2014-04-14 13:24:55 +0200

    Make tunnel bigger

--- blinkizeuch™/scenes/schwurbeltunnel/scene.glsl
+++ blinkizeuch™/scenes/schwurbeltunnel/scene.glsl
@@ -49,16 +49,17 @@ float schwurbelsaeule(vec3 p) {
 	float ra = length(p.xz);
 	float phi = atan(p.z, p.x);
 	float c = 4.;
+	float r = 6;
 
 	float phi1 = phi + TAU / 32. * p.y;
 	phi1 = mod(phi1, TAU /c) - TAU / c * .5;
 	vec3 p1 = vec3(ra * cos(phi1), p.y, ra * sin(phi1));
-	p1 = trans(p1, 4., 0., 0.);
+	p1 = trans(p1, r, 0., 0.);
 
 	float phi2 = phi - TAU / 32. * p.y;
 	phi2 = mod(phi2, TAU /c) - TAU / c * .5;
 	vec3 p2 = vec3(ra * cos(phi2), p.y, ra * sin(phi2));
-	p2 = trans(p2, 4., 0., 0.);
+	p2 = trans(p2, r, 0., 0.);
 
 	// actual schwurbel code
 	float height = 80.;
