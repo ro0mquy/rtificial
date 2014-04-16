ca3daf2f5055d6425a34145d068634f25c6e463e
Vincent Schüßler <vincent@vschuessler.org>
2014-04-16 03:36:56 +0200

    Integrate parts of camera and postproc

--- revision14/make.sh
+++ revision14/make.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-gcc -Os -s -fomit-frame-pointer -m32 -o main main.c 4klang.o libzeuch/libzeuch.a -I. $(sdl-config-32 --cflags --libs) $(pkg-config --cflags --libs glew) -Wall
+gcc -std=c99 -lm -Os -s -fomit-frame-pointer -m32 -o main main.c 4klang.o libzeuch/libzeuch.a -I. $(sdl-config-32 --cflags --libs) $(pkg-config --cflags --libs glew) -Wall
 
