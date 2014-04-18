180a55662505b84654035a29299c607fc2281321
ro0mquy <git@ro0mquy.de>
2014-04-18 22:24:41 +0200

    change background of ladebalken

--- blinkizeuch™/scenes/ladebalken/scene.glsl
+++ blinkizeuch™/scenes/ladebalken/scene.glsl
@@ -7,15 +7,15 @@ void main(void){
 
 	float progress = floor(progress * 10)/ 10;
 
-	vec3 color_backround = vec3(0.);
-	vec3 color_bar = vec3(1.);
+	vec3 color_backround = vec3(.01);
+	vec3 color_bar = vec3(.9);
 
 	vec3 final_color = color_backround;
 
 	if(pos.y > 0.45 && pos.y < 0.55 ){
 		if(pos.x < progress){
 			final_color = color_bar;
-			final_color *= .1 + smoothstep(0.45, 0.55, pos.y) * .9;
+			final_color *= .9;
 		}
 	}
 
