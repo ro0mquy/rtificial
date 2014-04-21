29c6059addd487c0c0511bdfe257a71aa7bc9ca2
Vincent Schüßler <vincent@vschuessler.org>
2014-04-21 12:08:14 +0200

    Re-export ufo scene

--- revision14/make.sh
+++ revision14/make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-gcc -std=c99 -lm -Os -s -fomit-frame-pointer -m32 -o main main.c 4klang.o libzeuch/libzeuch.a -I. $(sdl-config --cflags --libs) $(pkg-config --cflags --libs glew) -Wall -lm
+gcc -std=c99 -lm -Os -s -fomit-frame-pointer -m32 -o main main.c 4klang.o libzeuch/libzeuch.a -I. $(sdl-config-32 --cflags --libs) $(pkg-config --cflags --libs glew) -Wall -lm
 xz -f main
 cat header main.xz > rmk_hier_klicken
 chmod +x rmk_hier_klicken
