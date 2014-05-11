cc9417a3c5b3f3315cacc1254958459ad0c21383
Vincent Schüßler <vincent@vschuessler.org>
2014-05-11 12:23:09 +0200

    Spinning cube \o/ (in 4d)

--- blinkizeuch™/scenes/4d/scene.glsl
+++ blinkizeuch™/scenes/4d/scene.glsl
@@ -6,51 +6,102 @@ uniform float foo1;
 uniform float foo2;
 
 float intersect(vec3 origin, vec3 dir, vec3 v1, vec3 v2, vec3 v3);
+mat4 calc_matrix();
+vec4 cross4(vec4 a, vec4 b, vec4 c);
+mat4 rXY(float theta);
+mat4 rYZ(float theta);
+mat4 rZX(float theta);
+mat4 rXW(float theta);
+mat4 rYW(float theta);
+mat4 rZW(float theta);
 
 void main(void){
 	vec3 dir = get_direction();
 	vec3 final_color = vec3(0);
 
-	const vec3 p1 = vec3(-1, -1,  1);
-	const vec3 p2 = vec3( 1, -1,  1);
-	const vec3 p3 = vec3( 1, -1, -1);
-	const vec3 p4 = vec3(-1, -1, -1);
-	const vec3 p5 = vec3(-1,  1,  1);
-	const vec3 p6 = vec3( 1,  1,  1);
-	const vec3 p7 = vec3( 1,  1, -1);
-	const vec3 p8 = vec3(-1,  1, -1);
+	const vec4 p1  = vec4(-1, -1,  1, -1);
+	const vec4 p2  = vec4( 1, -1,  1, -1);
+	const vec4 p3  = vec4( 1, -1, -1, -1);
+	const vec4 p4  = vec4(-1, -1, -1, -1);
+	const vec4 p5  = vec4(-1,  1,  1, -1);
+	const vec4 p6  = vec4( 1,  1,  1, -1);
+	const vec4 p7  = vec4( 1,  1, -1, -1);
+	const vec4 p8  = vec4(-1,  1, -1, -1);
+	const vec4 p9  = vec4(-1, -1,  1,  1);
+	const vec4 p10 = vec4( 1, -1,  1,  1);
+	const vec4 p11 = vec4( 1, -1, -1,  1);
+	const vec4 p12 = vec4(-1, -1, -1,  1);
+	const vec4 p13 = vec4(-1,  1,  1,  1);
+	const vec4 p14 = vec4( 1,  1,  1,  1);
+	const vec4 p15 = vec4( 1,  1, -1,  1);
+	const vec4 p16 = vec4(-1,  1, -1,  1);
 
-	const vec3 triangles[] = vec3[](
+	const vec4 triangles[] = vec4[](
+		// first cube
 		p1, p4, p3, p1, p3, p2, // bottom
 		p5, p7, p8, p5, p6, p7, // top
 		p1, p5, p8, p1, p8, p4, // left
 		p2, p7, p6, p2, p3, p7, // right
 		p4, p8, p7, p4, p7, p3, // back
-		p1, p6, p5, p1, p2, p6  // front
+		p1, p6, p5, p1, p2, p6,  // front
+
+		// middle thing
+		//bottom connections
+		p1, p10, p9, p1, p2, p10,
+		p2, p11, p10, p2, p3, p11,
+		p3, p12, p11, p3, p4, p12,
+		p4, p9, p12, p4, p1, p9,
+		// middle connections
+		p1, p13, p5, p1, p9, p13,
+		p2, p14, p6, p2, p10, p14,
+		p3, p15, p7, p3, p11, p15,
+		p4, p16, p8, p4, p12, p16,
+		// top connections
+		p5, p14, p13, p5, p6, p14,
+		p6, p15, p14, p6, p7, p15,
+		p7, p16, p15, p7, p8, p16,
+		p8, p13, p16, p8, p5, p13,
+
+		// second cube
+		p9, p12, p11, p9, p11, p10, // bottom
+		p13, p15, p16, p13, p14, p15, // top
+		p9, p13, p16, p9, p16, p12, // left
+		p10, p15, p14, p10, p11, p15, // right
+		p12, p16, p15, p12, p15, p11, // back
+		p9, p14, p13, p9, p10, p14  // front
 	);
-	mat3 rotation = rY(radians(40.) * time) * rX(radians(30.) * time);
-	int n = 12;
+	float angle = radians(50.) * time;
+	mat4 rotation = rXY(angle) * rYZ(angle) * rZX(angle) * rXW(angle) * rYW(angle) * rZW(angle) * mat4(3.);
+	int n = 48;
 	int index = -1;
 	float t = 1e6; // far far away
+	mat4 projection = calc_matrix();
 	for(int i = 0; i < n; i++) {
-		vec3 v1 = rotation * triangles[3 * i];
-		vec3 v2 = rotation * triangles[3 * i + 1];
-		vec3 v3 = rotation * triangles[3 * i + 2];
+		vec3 v1 = (projection * rotation * triangles[3 * i]).xyz;
+		vec3 v2 = (projection * rotation * triangles[3 * i + 1]).xyz;
+		vec3 v3 = (projection * rotation * triangles[3 * i + 2]).xyz;
 		float t2 = intersect(view_position, dir, v1, v2, v3);
+		/*
 		if(t2 > 0. && t2 < t) {
 			index = i;
 			t = t2;
 		}
+		*/
+		if(t2 > 0.) {
+			final_color += .2;
+		}
 	}
+	mat4 view = calc_matrix();
 	if(index >= 0) {
 		int i = index;
-		vec3 v1 = rotation * triangles[3 * i];
-		vec3 v2 = rotation * triangles[3 * i + 1];
-		vec3 v3 = rotation * triangles[3 * i + 2];
-		vec3 normal = normalize(cross(v2 - v1, v3 - v1));
+		//vec3 v1 = /*rotation */ triangles[3 * i];
+		//vec3 v2 = /*rotation */ triangles[3 * i + 1];
+		//vec3 v3 = /*rotation */ triangles[3 * i + 2];
+		//vec3 normal = normalize(cross(v2 - v1, v3 - v1));
 		vec3 light = normalize(vec3(2, 3, 10) - view_position);
-		final_color = .1 + vec3(.5) * lambert(normal, light);
-		final_color += phong(normal, light, -dir, 32.);
+		//final_color = .1 + vec3(.5) * lambert(normal, light);
+		//final_color += phong(normal, light, -dir, 32.);
+		//final_color = vec3(1);
 	}
 
 	out_color.rgb = final_color;
@@ -75,4 +126,80 @@ float intersect(vec3 origin, vec3 dir, vec3 v1, vec3 v2, vec3 v3) {
 	if(v < 0. || u + v > 1.) return -1;
 	float t = dot(e2, Q) * inv_det;
 	return mix(-1, t, t > epilepsilon);
+}
+
+vec4 from = vec4(4, 0, 0, 0);
+vec4 to = vec4(0);
+vec4 up = vec4(0, 1, 0, 0);
+vec4 over = vec4(0, 0, 1, 0);
+mat4 calc_matrix() {
+	vec4 wa, wb, wc, wd;
+	wd = normalize(to - from);
+	wa = normalize(cross4(up, over, wd));
+	wb = normalize(cross4(over, wd, wa));
+	wc = cross4(wd, wa, wb);
+	return mat4(wa, wb, wc, wd);
+}
+
+vec4 cross4(vec4 a, vec4 b, vec4 c) {
+	return vec4(
+		dot(a.yzw, cross(b.yzw, c.yzw)),
+		-dot(a.xzw, cross(b.xzw, c.xzw)),
+		dot(a.xyw, cross(b.xyw, c.xyw)),
+		-dot(a.xyz, cross(b.xyz, c.xyz))
+	);
+}
+
+mat4 rXY(float theta) {
+	return mat4(
+		cos(theta), sin(theta), 0, 0,
+		-sin(theta), cos(theta), 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1
+	);
+}
+
+mat4 rYZ(float theta) {
+	return mat4(
+		1, 0, 0, 0,
+		0, cos(theta), sin(theta), 0,
+		0, -sin(theta), cos(theta), 0,
+		0, 0, 0, 1
+	);
+}
+
+mat4 rZX(float theta) {
+	return mat4(
+		cos(theta), 0, -sin(theta), 0,
+		0, 1, 0, 0,
+		sin(theta), 0, cos(theta), 0,
+		0, 0, 0, 1
+	);
+}
+
+mat4 rXW(float theta) {
+	return mat4(
+		cos(theta), 0, 0, sin(theta),
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		-sin(theta), 0, 0, cos(theta)
+	);
+}
+
+mat4 rYW(float theta) {
+	return mat4(
+		1, 0, 0, 0,
+		0, cos(theta), 0, -sin(theta),
+		0, 0, 1, 0,
+		0, sin(theta), 0, cos(theta)
+	);
+}
+
+mat4 rZW(float theta) {
+	return mat4(
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, cos(theta), -sin(theta),
+		0, 0, sin(theta), cos(theta)
+	);
 }
