3cbcdcfe10b53514707ba5a5072a07e3f725d68b
ro0mquy <git@ro0mquy.de>
2014-05-29 22:57:07 +0200

    implemented very simple raytracing for pebble

--- ro0mquy/wilderkiesel/src/wilderkiesel.c
+++ ro0mquy/wilderkiesel/src/wilderkiesel.c
@@ -1,71 +1,62 @@
 #include <pebble.h>
 #include <fix16.h>
+#include <fixvector3d.h>
 
-#define fb_w 144
-#define fb_h 168
+#define res_x 144
+#define res_y 168
 
 static Window *window;
 static Layer *canvas_layer;
 static AppTimer *redraw_timer;
 static time_t startup_time;
 
-static int8_t framebuffer[2][fb_w]; // buffer for two lines of pixels
-static uint8_t bitmap_data_framebuffer[fb_h][20]; // buffer for bitmap data, width has to be % 4 == 0
+static int8_t framebuffer[2][res_x]; // buffer for two lines of pixels
+static uint8_t bitmap_data_framebuffer[res_y][20]; // buffer for bitmap data, width has to be % 4 == 0
 static GBitmap bitmap_framebuffer;
+
+static const fix16_t fix16_two = F16(2);
+static uint8_t center_z = 1;
 
 static uint8_t fragment_draw(size_t x, size_t y, uint32_t ms) {
 	uint16_t out_color = 0;
 
-	/*
-	size_t pos_y = (y + ms / 10) % fb_h;
-	pos_y = abs(pos_y - fb_h / 2) * 2;
+	v3d* center = &(v3d) { .x = fix16_from_int(0), .y = fix16_from_int(0), .z = fix16_from_int(-center_z) };
+	fix16_t radius = fix16_from_int(1);
 
-	out_color = pos_y * 255 / fb_h;
-	*/
+	v3d* start = &(v3d) {
+		.x = fix16_from_int(0),
+		.y = fix16_from_int(0),
+		.z = fix16_from_int(0)
+	};
+	v3d* direction = &(v3d) {
+		.x = fix16_div(fix16_sub(fix16_from_int(x), F16(res_x / 2)), F16(res_y)),
+		.y = fix16_div(fix16_sub(fix16_from_int(y), F16(res_y / 2)), F16(res_y)),
+		.z = fix16_from_int(-1)
+	};
+	v3d_normalize(direction, direction);
 
-	fix16_t cx = fix16_div(fix16_from_int(10*x), fix16_from_int(fb_w));
-	fix16_t cy = fix16_div(fix16_from_int(10*y), fix16_from_int(fb_h));
-	fix16_t time = fix16_div(fix16_from_int(ms), fix16_from_int(10000));
-	fix16_t v = 0;
-	fix16_t fix16_two = F16(2);
+	v3d* relativ = &(v3d) { .x = fix16_from_int(0), .y = fix16_from_int(0), .z = fix16_from_int(0) };
+	v3d_sub(relativ, start, center);
 
-	v = fix16_add(v, fix16_sin(fix16_add(cx, time)));
-	v = fix16_add(v, fix16_sin(fix16_div(fix16_add(cy, time), fix16_two)));
-	v = fix16_add(v, fix16_sin(fix16_div(fix16_add(fix16_add(cx, cy), time), fix16_two)));
+	fix16_t discr1 = fix16_sq(v3d_dot(relativ, direction));
+	fix16_t discr2 = fix16_sub(v3d_dot(relativ, relativ), fix16_sq(radius));
+	fix16_t discriminant = fix16_sub(discr1, discr2);
 
-	cx = fix16_add(cx, fix16_sin(fix16_div(time, fix16_from_int(3))));
-	cy = fix16_add(cy, fix16_cos(fix16_div(time, fix16_two)));
-
-	v = fix16_add(v, fix16_sin(fix16_add(fix16_sqrt(fix16_add(fix16_add(fix16_sq(cx), fix16_sq(cy)), fix16_one)), time)));
-	v = fix16_div(v, fix16_two);
-
-	fix16_t color = fix16_div(fix16_add(fix16_sin(fix16_mul(fix16_pi, v)), fix16_one), fix16_two);
-	color = fix16_mul(color, fix16_from_int(256));
-	out_color = fix16_to_int(color);
-
-	/*
-	float cx = (float) x / fb_w;
-	float cy = (float) y / fb_h;
-	float time = ms / 1000.;
-	float v = 0.;
-	v += sin(cx+time);
-	v += sin((cy+time)/2.0);
-	v += sin((cx+cy+time)/2.0);
-	cx += sin(time/3.0);
-	cy += cos(time/2.0);
-	v += sin(sqrt(cx*cx+cy*cy+1.0)+time);
-	v = v/2.0;
-	out_color = sin(3.14159*v) * .5 + .5;
-	 */
+	// no solution, no hit
+	if (discriminant < 0) {
+		out_color = 255;
+	} else {
+		out_color = 0;
+	}
 
 	return out_color;
 }
 
 static void render(uint32_t milliseconds) {
-	memset(framebuffer, 0, 2 * fb_w);
+	memset(framebuffer, 0, 2 * res_x);
 	uint8_t active_line = 0;
-	for (size_t y = 0; y < fb_h; y++) {
-		for (size_t x = 0; x < fb_w; x++) {
+	for (size_t y = 0; y < res_y; y++) {
+		for (size_t x = 0; x < res_x; x++) {
 			// get color of pixel
 			// color from fragment_draw() is between 0 and 255
 			// range for dithering is between -128 and 127
@@ -73,10 +64,10 @@ static void render(uint32_t milliseconds) {
 			pixel_color += fragment_draw(x, y, milliseconds) - 128;
 			int16_t dithered_color = pixel_color / abs(pixel_color);
 			int16_t dithering_error = pixel_color - dithered_color * 127;
-			framebuffer[active_line][(x+1 + fb_w) % fb_w] += dithering_error * 7 / 16;
-			framebuffer[active_line ^ 1][(x-1 + fb_w) % fb_w] += dithering_error * 3 / 16;
+			framebuffer[active_line][(x+1 + res_x) % res_x] += dithering_error * 7 / 16;
+			framebuffer[active_line ^ 1][(x-1 + res_x) % res_x] += dithering_error * 3 / 16;
 			framebuffer[active_line ^ 1][x] += dithering_error * 5 / 16;
-			framebuffer[active_line ^ 1][(x+1) % fb_w] += dithering_error * 1 / 16;
+			framebuffer[active_line ^ 1][(x+1) % res_x] += dithering_error * 1 / 16;
 
 			// write to bitmap
 			size_t byte = x / 8;
@@ -87,7 +78,7 @@ static void render(uint32_t milliseconds) {
 			// modify bit of bitmap, yay bitoperations
 			bitmap_data_framebuffer[y][byte] = (bitmap_data_framebuffer[y][byte] & ~mask) | (-bw_dithered_color & mask);
 		}
-		memset(framebuffer[active_line], 0, fb_w);
+		memset(framebuffer[active_line], 0, res_x);
 		active_line ^= 1;
 	}
 }
@@ -110,6 +101,7 @@ static void redraw_task(void *data) {
 }
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+	center_z++;
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -147,7 +139,7 @@ static void init(void) {
 	window_set_fullscreen(window, true);
 	const bool animated = true;
 	window_stack_push(window, animated);
-	bitmap_framebuffer = (GBitmap) { .addr = bitmap_data_framebuffer, .bounds = GRect(0, 0, fb_w, fb_h), .row_size_bytes = 20 };
+	bitmap_framebuffer = (GBitmap) { .addr = bitmap_data_framebuffer, .bounds = GRect(0, 0, res_x, res_y), .row_size_bytes = 20 };
 	time(&startup_time);
 	redraw_timer = app_timer_register(16, redraw_task, NULL);
 }
