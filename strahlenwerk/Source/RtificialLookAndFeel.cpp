28d6220afd199cd35f89fc18112e4691475a3f66
Ps0ke <mail@ps0ke.de>
2014-07-24 02:37:20 +0200

    Added outlines to resizers

--- strahlenwerk/Source/RtificialLookAndFeel.cpp
+++ strahlenwerk/Source/RtificialLookAndFeel.cpp
@@ -1,11 +1,22 @@
 #include "RtificialLookAndFeel.h"
 
-RtificialLookAndFeel::RtificialLookAndFeel() {}
+RtificialLookAndFeel::RtificialLookAndFeel() {
+	setColour(RtificialLookAndFeel::outlineColourId, Colours::black);
+}
 
 RtificialLookAndFeel::~RtificialLookAndFeel() {}
 
 void RtificialLookAndFeel::drawStretchableLayoutResizerBar(Graphics& g, int w, int h, bool isVerticalBar, bool isMouseOver, bool isMouseDragging) {
 	if (isMouseOver || isMouseDragging){
-		g.fillAll(Colours::red.withAlpha(0.4f));
+		g.fillAll(findColour(RtificialLookAndFeel::outlineColourId));
+	}
+
+	g.setColour(findColour(RtificialLookAndFeel::outlineColourId));
+	if(isVerticalBar){
+		g.drawVerticalLine(0, 0, h);
+		g.drawVerticalLine(w-1, 0, h);
+	} else {
+		g.drawHorizontalLine(0, 0, w);
+		g.drawHorizontalLine(h-1, 0, w);
 	}
 }
--- strahlenwerk/Source/RtificialLookAndFeel.h
+++ strahlenwerk/Source/RtificialLookAndFeel.h
@@ -9,6 +9,10 @@ class RtificialLookAndFeel : public LookAndFeel_V3
 		RtificialLookAndFeel();
 		~RtificialLookAndFeel();
 
+		enum ColourIds{
+			outlineColourId
+		};
+
 		void drawStretchableLayoutResizerBar (Graphics& g, int w, int h, bool isVerticalBar, bool isMouseOver, bool isMouseDragging);
 };
 
