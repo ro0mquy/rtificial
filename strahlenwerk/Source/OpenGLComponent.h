defa62baac39d18d1fa50d77d4da37682fb6244d
ro0mquy <git@ro0mquy.de>
2014-08-04 13:27:38 +0200

    make OpenGl context 16:9

--- strahlenwerk/Source/OpenGLComponent.h
+++ strahlenwerk/Source/OpenGLComponent.h
@@ -10,12 +10,15 @@ class OpenGLComponent : public Component, private ApplicationCommandManagerListe
 		OpenGLComponent();
 		~OpenGLComponent();
 
+		void resized() override;
+		void paint(Graphics& g) override;
 		void applicationCommandInvoked(const ApplicationCommandTarget::InvocationInfo& info) override;
 		void applicationCommandListChanged() override;
 
 	private:
 		OpenGLContext context;
 		Renderer renderer;
+		Component fixedAspectRatioComponent;
 
 		JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (OpenGLComponent)
 };
