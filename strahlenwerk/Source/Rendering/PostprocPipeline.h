b8bc6cb753f0ca273e9797a36b2ed86e3ce4f398
Vincent Schüßler <vincent@vschuessler.org>
2014-08-12 17:11:08 +0200

    Move pipeline loading to separate loader class

--- strahlenwerk/Source/Rendering/PostprocPipeline.h
+++ strahlenwerk/Source/Rendering/PostprocPipeline.h
@@ -1,8 +1,6 @@
 #ifndef POSTPROCPIPELINE_H
 #define POSTPROCPIPELINE_H
 
-#include <string>
-#include <unordered_map>
 #include <vector>
 #include <memory>
 
@@ -14,13 +12,6 @@ class PostprocPipeline {
 		void render(SceneShader& shader);
 
 	private:
-		void loadShaders(OpenGLContext& context);
-		std::vector<std::vector<int>> loadMapping(const std::string& mappingSource);
-		std::vector<int> createOrder(const std::vector<std::vector<int>>& mapping);
-		void connectStages(const std::vector<int>& order, const std::vector<std::vector<int>>& mapping);
-		void insertBindings(const std::vector<int>& order, const std::vector<std::vector<int>>& inputPositions);
-
-		std::unordered_map<std::string, int> shaderIds;
 		std::vector<std::unique_ptr<PostprocShader>> shaders;
 };
 
---
+++ strahlenwerk/Source/Rendering/PostprocPipelineLoader.h
@@ -0,0 +1,27 @@
+#ifndef POSTPROCPIPELINELOADER_H
+#define POSTPROCPIPELINELOADER_H
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <memory>
+
+#include "PostprocShader.h"
+
+class PostprocPipelineLoader {
+	public:
+		std::vector<std::unique_ptr<PostprocShader>> load();
+
+	private:
+		void loadShaders(OpenGLContext& context);
+		std::vector<std::vector<int>> loadMapping(const std::string& mappingSource);
+		std::vector<int> createOrder(const std::vector<std::vector<int>>& mapping);
+		void connectStages(const std::vector<int>& order, const std::vector<std::vector<int>>& mapping);
+		void insertBindings(const std::vector<int>& order, const std::vector<std::vector<int>>& inputPositions);
+
+		std::unordered_map<std::string, int> shaderIds;
+		std::vector<std::unique_ptr<PostprocShader>> shaders;
+
+};
+
+#endif
