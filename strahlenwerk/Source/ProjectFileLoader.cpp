403d048598544ab3132394c8fd56b84006e1e078
Vincent Schüßler <vincent@vschuessler.org>
2014-08-14 12:10:56 +0200

    Implement ProjectFileLoader

--- strahlenwerk/Source/ProjectFileLoader.cpp
+++ strahlenwerk/Source/ProjectFileLoader.cpp
@@ -1,1 +1,32 @@
 #include "ProjectFileLoader.h"
+
+// TODO error handling
+
+ProjectFileLoader::ProjectFileLoader(std::string projectRoot) :
+	projectDir(projectRoot),
+	postprocDir(projectDir.getChildFile("postproc")),
+	sceneDir(projectDir.getChildFile("scenes"))
+{
+}
+
+std::vector<std::string> ProjectFileLoader::listPostprocFiles() {
+	return listFiles(postprocDir);
+}
+
+std::vector<std::string> ProjectFileLoader::listSceneFiles() {
+	return listFiles(sceneDir);
+}
+
+std::string ProjectFileLoader::getMappingFilePath() {
+	return projectDir.getChildFile("mapping.txt").getFullPathName().toStdString();
+}
+
+std::vector<std::string> ProjectFileLoader::listFiles(const File& dir) {
+	Array<File> results;
+	dir.findChildFiles(results, dir.findFiles, false);
+	std::vector<std::string> paths;
+	for(const auto& file : results) {
+		paths.emplace_back(file.getFullPathName().toStdString());
+	}
+	return paths;
+}
--- strahlenwerk/Source/ProjectFileLoader.h
+++ strahlenwerk/Source/ProjectFileLoader.h
@@ -3,15 +3,21 @@
 
 #include <string>
 #include <vector>
+#include <juce>
 
 class ProjectFileLoader {
 	public:
 		ProjectFileLoader(std::string projectRoot);
 
-		using File = int; // TODO
-		std::vector<File> getPostprocFiles();
-		std::vector<File> getSceneFiles();
-		File getMappingFile();
+		std::vector<std::string> listPostprocFiles();
+		std::vector<std::string> listSceneFiles();
+		std::string getMappingFilePath();
 
+	private:
+		static std::vector<std::string> listFiles(const File& dir);
+
+		const File projectDir;
+		const File postprocDir;
+		const File sceneDir;
 };
 #endif
