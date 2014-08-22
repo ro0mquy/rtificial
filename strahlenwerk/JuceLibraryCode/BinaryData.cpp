099c764ee2579254cdb1482c9d2a99eb0e13dab0
Vincent Schüßler <vincent@vschuessler.org>
2014-08-22 19:20:49 +0200

    Add JUCE BinaryData

---
+++ strahlenwerk/JuceLibraryCode/BinaryData.cpp
@@ -0,0 +1,40 @@
+/* ==================================== JUCER_BINARY_RESOURCE ====================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+namespace BinaryData
+{
+
+//================== juce ==================
+static const unsigned char temp_binary_data_0[] =
+"#include \"../JuceLibraryCode/JuceHeader.h\"\n";
+
+const char* juce = (const char*) temp_binary_data_0;
+
+
+const char* getNamedResource (const char*, int&) throw();
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes) throw()
+{
+    unsigned int hash = 0;
+    if (resourceNameUTF8 != 0)
+        while (*resourceNameUTF8 != 0)
+            hash = 31 * hash + (unsigned int) *resourceNameUTF8++;
+
+    switch (hash)
+    {
+        case 0x0031f2ed:  numBytes = 43; return juce;
+        default: break;
+    }
+
+    numBytes = 0;
+    return 0;
+}
+
+const char* namedResourceList[] =
+{
+    "juce"
+};
+
+}
