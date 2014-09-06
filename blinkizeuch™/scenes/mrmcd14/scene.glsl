804c1f6e3170f6fecd6b77d208cf6c6534c5f218
ro0mquy <git@ro0mquy.de>
2014-09-06 18:39:50 +0200

    very important and groundbreaking changes to mrmcd14

--- blinkizeuch™/scenes/mrmcd14/scene.glsl
+++ blinkizeuch™/scenes/mrmcd14/scene.glsl
@@ -10,24 +10,66 @@ uniform vec3 color_foo2;
 uniform float foo1;
 uniform float foo2;
 
+vec3 c[19] = vec3[](
+	vec3(1., 0., 0.),
+	vec3(0., 1., 0.),
+	vec3(0., 0., 1.),
+	vec3(.577, .577, .577),
+	vec3(-.577, .577, .577),
+	vec3(.577, -.577, .577),
+	vec3(.577, .577, -.577),
+	vec3(0., .357, .934),
+	vec3(0., -.357, .934),
+	vec3(.934, 0., .357),
+	vec3(-.934, 0., .357),
+	vec3(.357, .934, 0.),
+	vec3(-.357, .934, 0.),
+	vec3(0., .851, .526),
+	vec3(0., -.851, .526),
+	vec3(.526, 0., .851),
+	vec3(-.526, 0., .851),
+	vec3(.851, .526, 0.),
+	vec3(-.851, .526, 0.)
+	);
+
+
 void main(void){
 	vec3 dir = get_direction();
 	vec3 final_color = vec3(0);
 	int i;
 	vec3 hit = march(view_position, dir, i);
-	if(i < 100){
+	//if(i < 100){
 		vec3 normal = calc_normal(hit);
 		//vec3 normal = normalize(df(hit).yzw);
 		//float lamb = .95 * lambert(vec3(1., 2., 1.), normal) + .05;
-		Material material = Material(vec3(0., .3, 1.), foo1, foo2);
-		SphereLight light = SphereLight(vec3(10.), vec3(1.), 2., 200.);
-		SphereLight zweiteslight = SphereLight(vec3(2., -1., -8.), vec3(1.), .5, 200.);
+		float roughness = foo1 + smooth_noise(10. * hit + 5. * sin(hit.x * 20.)) * .4;
+		Material material = Material(vec3(0., .3, 1.), roughness, foo2);
+		SphereLight light = SphereLight(vec3(10.), color_foo1, 2., 200.);
+		SphereLight zweiteslight = SphereLight(vec3(2., -2., 8.), color_foo2, .5, 200.);
 		final_color = apply_light(hit, normal, -dir, material, light);
-		//final_color += apply_light(hit, normal, -dir, material, zweiteslight);
+		final_color += apply_light(hit, normal, -dir, material, zweiteslight);
 		vec3 ambient_color = mix(color_foo1, color_foo2, normal.z * .5 + .5);
 		final_color = mix(final_color, ambient_color, .01);
-	}
-	out_color.rgb = final_color;
+	//}
+	out_color.rgb = final_color + 1./512. * vec3( // so schön weerboß
+			classic_noise(gl_FragCoord.xy),
+			classic_noise(gl_FragCoord.xy + 30.),
+			classic_noise(gl_FragCoord.xy + 70.)
+			);
+}
+
+// This will generate the distance function for some kind of spikeball.
+// It's a bit magic - it's based on the paper "Generalized Distance Functions"
+// - don't ask - play.
+float spikeball(vec3 p){
+	float l = length(p);
+	p = normalize(p);
+	float b = 0.;
+	for (int i = 3; i < 19; i++)
+		b = max(abs(dot(p, c[i])), b);
+	b = 1. - acos(b - .01)/(acos(-1.) * .5);
+	b = smoothstep(.78, 1., b);
+	return l - 2.2 * pow(1.5, b);
 }
 
 vec2 f(vec3 p){
@@ -37,7 +79,10 @@ vec2 f(vec3 p){
 	//float box2 = roundbox(rX(radians(30.)) * trans(p, 3., 3., 3.), vec3(2.), .1);
 	//float fbox = mix(box1, box2, sin(time * 25.) * .03 + .5);
 	//float fbox = torus(rZ(radians(30.) * time) * rX(radians(20.) * time) * p, vec2(2., .5));
-	float fbox = sphere(p, 2.);
+	//float fbox = sphere(p, 2.);
+	//float fbox = scale(spikeball, trans(p, 0., 0., -5.), .5);
+	float fbox = sphere(trans(p, 0., 0., -5.), 2.);
+	fbox = min(fbox, -sphere(p, 50.));
 	return vec2(fbox, 1);
 }
 
