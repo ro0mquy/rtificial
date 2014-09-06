5c5c905c274f1a81c7300d4e69776a2ab230e10a
ro0mquy <git@ro0mquy.de>
2014-09-06 12:54:44 +0200

    more light magic from _vincent

--- libblink/libblink.glsl
+++ libblink/libblink.glsl
@@ -468,9 +468,14 @@ struct Material {
 
 struct SphereLight {
 	vec3 center;
+	vec3 color;
 	float radius;
 	float intensity;
 };
+
+float square(float a) {
+	return a * a;
+}
 
 vec3 apply_light(vec3 p, vec3 N, vec3 V, Material material, SphereLight light) {
 	vec3 L = light.center - p;
@@ -485,20 +490,14 @@ vec3 apply_light(vec3 p, vec3 N, vec3 V, Material material, SphereLight light) {
 	float PI = acos(-1.);
 	vec3 H = .5 * (L_spec + V);
 	float NdotH = dot(N, H);
-	float NdotLspec = pdot(N, L_spec);
+	float NdotL= pdot(N, L_spec);
 	float NdotV = dot(N, V);
-	float alpha = material.roughness * material.roughness;
-	float alpha2 = alpha * alpha;
-	float stuff = NdotH * NdotH * (alpha2 - 1.) + 1.;
-	float k = material.roughness + 1.;
-	k *= k;
-	float specular_without_F = NdotLspec * alpha2 / (4. * mix(NdotLspec, 1., k) * mix(NdotV, 1., k) * PI * stuff * stuff);
+	float alpha = square(material.roughness);
+	float alpha2 = square(alpha);
+	float k = square(material.roughness + 1.);
+	float specular_without_F = NdotL * alpha2 / (4. * mix(NdotL, 1., k) * mix(NdotV, 1., k) * PI * square(square(NdotH) * (alpha2 - 1.) + 1.));
 	float alphastrich = clamp(alpha + light.radius * .5 / length(closestPoint), 0., 1.);
 	float spec_norm = alpha2 / (alphastrich * alphastrich);
-	specular_without_F *= spec_norm;
-
-	vec3 Lnorm = normalize(L);
-	float NdotL = pdot(L, N);
 
 	float VdotH = dot(V, H);
 	float fresnel_power = exp2((-5.55473 * VdotH - 6.98316) * VdotH);
@@ -507,14 +506,10 @@ vec3 apply_light(vec3 p, vec3 N, vec3 V, Material material, SphereLight light) {
 
 	vec3 dielectric_color = .5 * (material.color / PI * NdotL + specular_without_F * F_dielectric);
 	vec3 metal_color = specular_without_F * F_metal;
-	float light_distance = distance(p, light.center);
-	float foo = light_distance / light.radius;
-	foo *= foo;
-	foo *= foo;
-	foo = 1. - foo;
-	foo *= foo;
-	float falloff = clamp(foo, 0., 1.) / (light_distance * light_distance + 1.);
-	return mix(dielectric_color, metal_color, material.metallic) * falloff * light.intensity;
+	float light_distance = distance(p, closestPoint);
+	float foo = square(1. - square(square(light_distance / light.radius)));
+	float falloff = clamp(foo, 0., 1.) / (square(light_distance) + 1.);
+	return mix(dielectric_color, metal_color, material.metallic) * falloff * light.intensity * light.color * spec_norm;
 }
 
 #line 1
