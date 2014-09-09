1d9091d9dfe0b6b370f18c55edf4944b57ffb456
ro0mquy <git@ro0mquy.de>
2014-09-09 23:47:17 +0200

    improve conicbobbels

--- deadline14/scenes/conicbobbel.glsl
+++ deadline14/scenes/conicbobbel.glsl
@@ -3,13 +3,18 @@
 #line 4
 
 uniform vec3 conic_bobbel_color; // color
+uniform vec3 conic_ring_color; // color
 
-uniform float conic_smooth_factor; //float
+uniform float conic_smooth_factor; // float
+uniform float manual_time; // float
+uniform float conic_ring_metalic; // float
+uniform float conic_ring_roughness; // float
 
 vec3 colors[] = vec3[](
-		conic_bobbel_color,
 		vec3(.03, .0, .0),
-		vec3(.0)
+		vec3(.0),
+		conic_bobbel_color,
+		conic_ring_color
 		);
 
 void main(void) {
@@ -25,13 +30,15 @@ void main(void) {
 
 		Material mat;
 		if(material == 0.) {
-			mat = Material(colors[material], 0.2, 1.);
-		} else if(material == 1.) {
 			float size = 2.;
 			float stripes = mod(floor(hit.x / size), 2.);
 			mat = Material(colors[material] * stripes, 0.5, 0.);
-		} else if(material == 2.) {
+		} else if(material == 1.) {
 			mat = Material(colors[material], 1., 0.);
+		} else if (material == 3.) {
+			mat = Material(colors[material], conic_ring_roughness, conic_ring_metalic);
+		} else {
+			mat = Material(colors[material], 0.2, 1.);
 		}
 		color = apply_light(hit, normal, -direction, mat, SphereLight(vec3(5., 9., 10.), vec3(1.), 2., 100.));
 	}
@@ -43,26 +50,36 @@ vec2 f(vec3 p) {
 	vec3 p_bobbel = trans(p, -1., 0., -3.);
 	float f_bobbel = p.y;
 
-	//float time = 7.5;
-	vec3 p_cone = trans(p_bobbel, 2 * time, 0., 0.);
+	//float time = 10. * manual_time;
+	vec3 p_cone = trans(p_bobbel, 10. * time, 0., 0.);
 	p_cone.x = mod(p_cone.x, 10.) - 5.;
 	float f_cone = p.y;
 	float line_radius = mix(.1, .5, smoothstep(0., 2., p_cone.x));
 	f_cone = line(p_cone, vec3(2., 0., 0.), vec3(0., 0., 0.), line_radius);
 
-	vec3 p_ring = trans(p_bobbel, 0., 0., 0.);
-	float domrep_width = 5.;
-	p_ring.x = mod(p_bobbel.x, domrep_width) - domrep_width / 2.;
+	float T = 1.;
+	vec3 p_ring = trans(p_cone, 2. - 3. * mod(time, T) / T, 0., 0.);
 	float f_ring = p.y;
-	float ring_radius = 1.5 * impulse(30., mod(2 * time, domrep_width) / domrep_width);
-	f_ring = abs(sphere(p_ring, ring_radius)) - .01 * ring_radius;
+	float ring_radius = 1.5 * impulse(10., mod(time, T) / T);
+	f_ring = abs(sphere(p_ring, ring_radius)) - .2 * ring_radius;
 	f_ring = smax(f_ring, abs(p_ring.x) - .03, .02);
 
-	f_bobbel = smin(f_cone, f_ring, conic_smooth_factor);
+	vec3 p_ring2 = trans(p_cone, 2. - 3. * mod(time + T/2.5, T) / T, 0., 0.);
+	float f_ring2 = p.y;
+	float ring_radius2 = 1.5 * impulse(10., mod(time + T/2.5, T) / T);
+	f_ring2 = abs(sphere(p_ring2, ring_radius2)) - .2 * ring_radius2;
+	f_ring2 = smax(f_ring2, abs(p_ring2.x) - .03, .02);
+	f_ring = min(f_ring, f_ring2);
+
+	//f_bobbel = smin(f_cone, f_ring, conic_smooth_factor);
 	//f_bobbel = f_ring;
 
-	vec2 m_bobbel = vec2(f_bobbel, 0.);
-	vec2 bottom = vec2(p.y + 2., 1.);
-	vec2 bounding = vec2(-sphere(p - camera_position, 50.), 2.);
+	vec2 m_cone = vec2(f_cone, 2.);
+	vec2 m_ring = vec2(f_ring, 3.);
+	vec2 m_bobbel = smin_material(m_cone, m_ring, conic_smooth_factor);
+
+	//vec2 m_bobbel = vec2(f_bobbel, 2.);
+	vec2 bottom = vec2(p.y + 2., 0.);
+	vec2 bounding = vec2(-sphere(p - camera_position, 50.), 1.);
 	return min_material(m_bobbel, min_material(bottom, bounding));
 }
