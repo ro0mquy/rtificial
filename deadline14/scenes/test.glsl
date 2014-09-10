58fca53870d506cc02dab83bc06c6da497e2d247
Vincent Schüßler <vincent@vschuessler.org>
2014-09-10 12:28:13 +0200

    Remove test scene

--- deadline14/scenes/test.glsl
+++
@@ -1,8 +0,0 @@
-#include "scene_head.glsl"
-#include "rtificial.glsl"
-
-void main() {
-	vec3 color = vec3(gl_FragCoord.xy / res, 0.);
-	color += 10. * step(distance(gl_FragCoord.xy / res, vec2(.5)), .1);
-	output_color(color, 0.);
-}
