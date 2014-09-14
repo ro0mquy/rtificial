adc0a4d71c49e7be8e0919d0bb8366f80b8db6bb
ro0mquy <git@ro0mquy.de>
2014-09-14 23:04:14 +0200

    update Sidebar

--- strahlenwerk/Source/Sidebar/Sidebar.cpp
+++ strahlenwerk/Source/Sidebar/Sidebar.cpp
@@ -1,7 +1,6 @@
 #include "Sidebar.h"
 #include <Timeline/TimelineData.h>
 #include <Timeline/Interpolator.h>
-#include <Timeline/TreeIdentifiers.h>
 #include <Timeline/ValueEditorPropertyComponent.h>
 
 Sidebar::Sidebar() :
@@ -16,11 +15,10 @@ void Sidebar::updateProperties() {
 	Interpolator& interpolator = data.getInterpolator();
 	Array<PropertyComponent*> properties;
 
-	ValueTree uniformsArray = data.getUniformsArray();
-	const int numUniforms = uniformsArray.getNumChildren();
+	const int numUniforms = data.getNumUniforms();
 	for (int i = 0; i < numUniforms; i++) {
-		ValueTree uniform = uniformsArray.getChild(i);
-		const String name = uniform.getProperty(treeId::uniformName);
+		ValueTree uniform = data.getUniform(i);
+		const String name = data.getUniformName(uniform);
 
 		auto uniformState = interpolator.getCurrentUniformValue(uniform);
 		ValueTree value = uniformState.first;
