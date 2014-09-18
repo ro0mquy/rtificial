1cf3f1d8e7c122edc22a0a279be11af7996f071a
Vincent Schüßler <vincent@vschuessler.org>
2014-09-18 21:57:24 +0200

    Add missing headers

--- strahlenwerk/JuceLibraryCode/JuceHeader.h
+++ strahlenwerk/JuceLibraryCode/JuceHeader.h
@@ -34,11 +34,13 @@
  using namespace juce;
 #endif
 
+#if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
     const char* const  projectName    = "strahlenwerk";
     const char* const  versionString  = "";
     const int          versionNumber  = 0x0;
 }
+#endif
 
 #endif   // __APPHEADERFILE_GXTJTP__
