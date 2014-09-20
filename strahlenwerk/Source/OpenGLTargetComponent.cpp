062758539eb7784306669d4414bfbc39611747c0
Vincent Schüßler <vincent@vschuessler.org>
2014-09-20 11:59:45 +0200

    Add border to overlay

--- strahlenwerk/Source/OpenGLTargetComponent.cpp
+++ strahlenwerk/Source/OpenGLTargetComponent.cpp
@@ -5,16 +5,27 @@
 
 void OpenGLTargetComponent::paint(Graphics& g) {
 	if(StrahlenwerkApplication::getInstance()->getProperties().getBoolValue(PropertyNames::GRID_ENABLED)) {
-		// TODO remove from border
 		const auto bounds = getLocalBounds();
 		const float thirdHoriz = bounds.getWidth() / 3.;
 		const float thirdVert = bounds.getHeight() / 3.;
 		const float thickness = bounds.getWidth() / 200.;
+		const float border = .05;
+		const auto reducedBounds = bounds.reduced(bounds.getWidth() * border, bounds.getHeight() * border);
+
+		Path path;
+		path.startNewSubPath(reducedBounds.getX(), thirdVert);
+		path.lineTo(reducedBounds.getRight(), thirdVert);
+		path.startNewSubPath(reducedBounds.getX(), thirdVert * 2);
+		path.lineTo(reducedBounds.getRight(), thirdVert * 2);
+		path.startNewSubPath(thirdHoriz, reducedBounds.getY());
+		path.lineTo(thirdHoriz, reducedBounds.getBottom());
+		path.startNewSubPath(thirdHoriz * 2, reducedBounds.getY());
+		path.lineTo(thirdHoriz * 2, reducedBounds.getBottom());
+		path.addRectangle(reducedBounds);
+
 		g.setColour(Colours::white);
 		g.setOpacity(.3);
-		g.drawLine(0., thirdVert, bounds.getWidth(), thirdVert, thickness);
-		g.drawLine(0., thirdVert * 2, bounds.getWidth(), thirdVert * 2, thickness);
-		g.drawLine(thirdHoriz, bounds.getHeight(), thirdHoriz, 0., thickness);
-		g.drawLine(thirdHoriz * 2, bounds.getHeight(), thirdHoriz * 2, 0., thickness);
+		g.strokePath(path, PathStrokeType(thickness));
+
 	}
 }
