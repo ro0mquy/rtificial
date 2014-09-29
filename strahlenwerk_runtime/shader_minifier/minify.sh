baa9246f8f0d6714135cef1b6678b6ffb944692e
Vincent Schüßler <vincent@vschuessler.org>
2014-09-29 17:24:30 +0200

    Implement new verheaderer

--- strahlenwerk_runtime/shader_minifier/minify.sh
+++ strahlenwerk_runtime/shader_minifier/minify.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+function verheader {
+	local TARGET="minified/""$1"".h"
+	echo "#ifndef ""$1""_H" > $TARGET
+	echo "#define ""$1""_H" >> $TARGET
+	echo -n "const char ""$1""_source[] = R\"shader_source(" >> $TARGET
+	cat shader_code.h >> $TARGET
+	echo ")shader_source\";" >> $TARGET
+	echo "#endif" >> $TARGET
+	rm shader_code.h
+}
+
 DIR=$1
 
 mkdir -p minified
 
 for f in $(find $DIR -name '*.glsl'); do
-	mono shader_minifier.exe --format none -v $f && mv shader_code.h minified/$(basename $f)
+	mono shader_minifier.exe --format none -v $f && verheader $(basename -s .glsl $f)
 done
