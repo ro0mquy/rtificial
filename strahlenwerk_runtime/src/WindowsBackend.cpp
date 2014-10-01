cad24fcbe12cb0d133ef5c0b3bb11f9a249ce687
Ps0ke <mail@ps0ke.de>
2014-10-01 15:03:51 +0200

    Added gl function loading to strahlenwerk_runtime

--- strahlenwerk_runtime/src/WindowsBackend.cpp
+++ strahlenwerk_runtime/src/WindowsBackend.cpp
@@ -1,7 +1,41 @@
 #ifdef BUILD_WINDOWS
 #include "Backend.h"
+#include "gl_indentifiers.h"
 
 void WindowsBackend::init(int width, int height, bool fullscreen) {
+	// Load OpenGL functions manually
+	glActiveTexture = (PFNGLACTIVETEXTUREPROC)wglGetProcAddress("glActiveTexture");
+	glGenFramebuffers = (PFNGLGENBUFFERSPROC)wglGetProcAddress("glGenFramebuffers");
+	glGenTextures = (PFNGLGENTEXTURESPROC)wglGetProcAddress("glGenTextures");
+	glBindFramebuffer = (PFNGLBINDFRAMEBUFFERPROC)wglGetProcAddress("glBindFramebuffer");
+	glBindTexture = (PFNGLBINDTEXTUREPROC)wglGetProcAddress("glBindTexture");
+	glTexImage2D = (PFNGLTEXIMAGE2DPROC)wglGetProcAddress("glTexImage2D");
+	glTexParameteri = (PFNGLTEXPARAMETERIPROC)wglGetProcAddress("glTexParameteri");
+	glGenerateMipmap = (PFNGLGENERATEMIPMAPPROC)wglGetProcAddress("glGenerateMipmap");
+	glFramebufferTexture2D = (PFNGLFRAMEBUFFERTEXTURE2DPROC)wglGetProcAddress("glFramebufferTexture2D");
+	glDrawBuffers = (PFNGLDRAWBUFFERSPROC)wglGetProcAddress("glDrawBuffers");
+	glViewport = (PFNGLVIEWPORTPROC)wglGetProcAddress("glViewport");
+	glDeleteTextures = (PFNGLDELETETEXTURESPROC)wglGetProcAddress("glDeleteTextures");
+	glDeleteFramebuffers = (PFNGLDELETEFRAMEBUFFERSPROC)wglGetProcAddress("glDeleteFramebuffers");
+	glEnable = (PFNGLENABLEPROC)wglGetProcAddress("glEnable");
+	glDisable = (PFNGLDISABLEPROC)wglGetProcAddress("glDisable");
+	glCreateShader = (PFNGLCREATESHADERPROC)wglGetProcAddress("glCreateShader");
+	glShaderSource = (PFNGLSHADERSOURCEPROC)wglGetProcAddress("glShaderSource");
+	glCompileShader = (PFNGLCOMPILESHADERPROC)wglGetProcAddress("glCompileShader");
+	glCreateProgram = (PFNGLCREATEPROGRAMPROC)wglGetProcAddress("glCreateProgram");
+	glAttachShader = (PFNGLATTACHSHADERPROC)wglGetProcAddress("glAttachShader");
+	glDeleteShader = (PFNGLDELETESHADERPROC)wglGetProcAddress("glDeleteShader");
+	glLinkProgram = (PFNGLLINKPROGRAMPROC)wglGetProcAddress("glLinkProgram");
+	glUseProgram = (PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram");
+	glTexParameterf = (PFNGLTEXPARAMETERFPROC)wglGetProcAddress("glTexParameterf");
+	glClear = (PFNGLCLEARPROC)wglGetProcAddress("glClear");
+	glEnableVertexAttribArray = (PFNGLENABLEVERTEXATTRIBARRAYPROC)wglGetProcAddress("glEnableVertexAttribArray");
+	glVertexAttribPointer = (PFNGLVERTEXATTRIBPOINTERPROC)wglGetProcAddress("glVertexAttribPointer");
+	glUniform2f = (PFNGLUNIFORM2FPROC)wglGetProcAddress("glUniform2f");
+	glDrawArrays = (PFNGLDRAWARRAYSPROC)wglGetProcAddress("glDrawArrays");
+	glDisableVertexAttribArray = (PFNGLDISABLEVERTEXATTRIBARRAYPROC)wglGetProcAddress("glDisableVertexAttribArray");
+	glDeleteProgram = (PFNGLDELETEPROGRAMPROC)wglGetProcAddress("glDeleteProgram");
+
 }
 
 void WindowsBackend::play_music(int16_t* audio) {
