00079db8c458e1040a95a8613b2537d22d900304
Ps0ke <mail@ps0ke.de>
2014-10-02 23:42:45 +0200

    Add bobble into spalt

--- deadline14/scenes/syntakischerspalt.glsl
+++ deadline14/scenes/syntakischerspalt.glsl
@@ -1,6 +1,7 @@
 #include "scene_head.glsl"
 #include "rtificial.glsl"
-#line 4
+#include "bobbel.glsl"
+#line 5
 
 uniform vec3 synapse_color; // color
 uniform float synapse_gap; // float
@@ -11,13 +12,16 @@ uniform float synapse_dd; // float
 uniform float synapse_dist_fog_a; // float
 uniform float synapse_dist_fog_b; // float
 
-Material materials[2] = Material[2](
+Material materials[3] = Material[3](
 	Material(vec3(1.), .5, 0.),
-	Material(synapse_color, 0.7, 1.)
+	Material(synapse_color, 0.7, 1.),
+	Material(vec3(0.,1.,0.), .2, 1.)
 );
 
 const float MATERIAL_ID_BOUNDING = 0.;
-const float MATERIAL_ID_SYNAPSE = 1.;
+const float MATERIAL_ID_SYNAPSE  = 1.;
+const float MATERIAL_ID_BOBBEL   = 2.;
+
 
 void main(void) {
 	vec3 direction = get_direction();
@@ -43,8 +47,8 @@ vec2 f(vec3 p) {
 	// domrep with rotation
 	vec3 dr_factor = vec3(50.);
 	vec3 q = domrepv(p, dr_factor);
-	q *= rZ(2*vnoise(3*floor(p/dr_factor)));
-	q *= rY(1.5*vnoise(floor(p/dr_factor)));
+	q *= rZ(2*rand(3*floor(p/dr_factor)));
+	q *= rY(1.5*rand(floor(p/dr_factor)));
 
 	// mirror
 	q.x = abs(q.x);
@@ -61,6 +65,15 @@ vec2 f(vec3 p) {
 	float capsule = line(q, vec3(2.,0.,0.), vec3(20.,0.,0.), mix(synapse_aa, synapse_bb, smoothstep(10.*synapse_cc, 10*synapse_dd, q.x)));
 
 	vec2 synapse = vec2(smin(smax(-sphere1, sphere2, 1.), capsule, 1.), MATERIAL_ID_SYNAPSE);
+
+	// bobbel
+	vec3 p_bbl = p;
+	p_bbl = trans(p_bbl, 23., -25., -23.);
+	p_bbl *= rY(-0.1*TAU);
+	float scale_factor = 0.15;
+	float bbl = conicbobbel(p_bbl / scale_factor, 2.) * scale_factor;
+	vec2 bobbel = vec2(bbl, MATERIAL_ID_BOBBEL);
+
 	vec2 bounding = vec2(-sphere(p - camera_position, -synapse_dist_fog_a), MATERIAL_ID_BOUNDING);
-	return min_material(synapse, bounding);
+	return min_material(synapse, min_material(bounding, bobbel));
 }
