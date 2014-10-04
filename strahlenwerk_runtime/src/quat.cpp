e70b4dc24cd4bece47468300683b30c1c78257cf
Ps0ke <mail@ps0ke.de>
2014-10-04 09:58:16 +0200

    fix nan in quat.cpp

--- strahlenwerk_runtime/src/quat.cpp
+++ strahlenwerk_runtime/src/quat.cpp
@@ -1,5 +1,6 @@
 #include "quat.h"
 #include <cmath>
+#include "scalar.h"
 
 quat::quat() :
 	quat(1., 0., 0., 0.)
@@ -70,7 +71,18 @@ quat slerp(quat const& q1, quat const& q2, const double& t) {
 		cosTheta = -cosTheta;
 	}
 
+	cosTheta = cosTheta < 0. ? 0. : cosTheta > 1. ? 1. : cosTheta;
 	double angle = acos(cosTheta);
+
+	if (abs(angle) < .001) {
+		return normalize(quat(
+			mix(q1.w, q2_.w, t),
+			mix(q1.x, q2_.x, t),
+			mix(q1.y, q2_.y, t),
+			mix(q1.z, q2_.z, t)
+			));
+	}
+
 	return (sin((1. - t) * angle) * q1 + sin(t * angle) * q2_) * (1.0 / sin(angle));
 }
 
