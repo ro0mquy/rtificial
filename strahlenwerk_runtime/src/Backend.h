f4d7fcafb7fa811fc29bc38d2cd8e109e3373d1d
Ps0ke <mail@ps0ke.de>
2014-10-11 16:41:21 +0200

    Remove unnecessary empty backend class

--- strahlenwerk_runtime/src/Backend.h
+++ strahlenwerk_runtime/src/Backend.h
@@ -3,18 +3,6 @@
 
 using int16_t = short;
 
-// interface for backends
-class EmptyBackend {
-	void init(int width, int height, bool fullscreen);
-	void initAudio(bool threaded);
-	void playAudio();
-	double geTime();
-	// return false for abort
-	bool beforeFrame();
-	void afterFrame();
-	void cleanup();
-};
-
 #ifdef _WINDOWS
 
 #include <windows.h>
