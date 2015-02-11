c9acf2ee8df56175dcf8af9e587dabab35587858
ro0mquy <git@ro0mquy.de>
2015-02-11 09:45:00 +0100

    don't add tree to selection if it is already selected

--- strahlenwerk/Source/Timeline/Selection.cpp
+++ strahlenwerk/Source/Timeline/Selection.cpp
@@ -12,12 +12,19 @@ ValueTree* Selection::operator[](const int index) {
 }
 
 void Selection::set(ValueTree tree) {
+	if (selectedTrees.size() == 1 && contains(tree)) {
+		// tree is already the only selected value tree
+		return;
+	}
 	selectedTrees.clear();
 	selectedTrees.add(new ValueTree(tree));
 	sendChangeMessage();
 }
 
 void Selection::add(ValueTree tree) {
+	if (contains(tree)) {
+		return;
+	}
 	selectedTrees.add(new ValueTree(tree));
 	sendChangeMessage();
 }
@@ -25,4 +32,14 @@ void Selection::add(ValueTree tree) {
 void Selection::clear() {
 	selectedTrees.clear();
 	sendChangeMessage();
+}
+
+bool Selection::contains(ValueTree treeToCheckFor) {
+	const int numTrees = selectedTrees.size();
+	for (int i = 0; i < numTrees; i++) {
+		if (*selectedTrees.getUnchecked(i) == treeToCheckFor) {
+			return true;
+		}
+	}
+	return false;
 }
--- strahlenwerk/Source/Timeline/Selection.h
+++ strahlenwerk/Source/Timeline/Selection.h
@@ -14,6 +14,7 @@ class Selection :
 		void set(ValueTree tree);
 		void add(ValueTree tree);
 		void clear();
+		bool contains(ValueTree treeToCheckFor);
 
 	private:
 		OwnedArray<ValueTree> selectedTrees;
