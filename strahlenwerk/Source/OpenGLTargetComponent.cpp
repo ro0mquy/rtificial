9e931d1afba42e73dd5e05639985ce86f647a6c1
Vincent Schüßler <v.schuessler@gmail.com>
2015-02-14 14:37:49 +0100

    Add axis orientation helper to 3d view (fix #77)

--- strahlenwerk/Source/OpenGLTargetComponent.cpp
+++ strahlenwerk/Source/OpenGLTargetComponent.cpp
@@ -3,19 +3,26 @@
 #include "StrahlenwerkApplication.h"
 #include "PropertyNames.h"
 #include "Renderer.h"
+#include "Timeline/Interpolator.h"
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+using namespace glm;
 
 OpenGLTargetComponent::OpenGLTargetComponent(Renderer& _renderer) : renderer(_renderer) {
 	setOpaque(true);
 }
 
 void OpenGLTargetComponent::paint(Graphics& g) {
-	const auto bounds = getLocalBounds();
+	const Rectangle<int> bounds = getLocalBounds();
 	if(StrahlenwerkApplication::getInstance()->getProperties().getBoolValue(PropertyNames::GRID_ENABLED)) {
 		const float thirdHoriz = bounds.getWidth() / 3.;
 		const float thirdVert = bounds.getHeight() / 3.;
 		const float thickness = bounds.getWidth() / 200.;
 		const float border = .05;
-		const auto reducedBounds = bounds.reduced(bounds.getWidth() * border, bounds.getHeight() * border);
+		const Rectangle<int> reducedBounds = bounds.reduced(bounds.getWidth() * border, bounds.getHeight() * border);
 
 		Path path;
 		path.startNewSubPath(0, thirdVert);
@@ -40,4 +47,36 @@ void OpenGLTargetComponent::paint(Graphics& g) {
 	const double frameTimeInMillis = frameTime / 1000000.;
 	const String frameTimeString = String(frameTimeInMillis) + "ms\n" + String(1000. / frameTimeInMillis) + "fps";
 	g.drawMultiLineText(frameTimeString, 10, 12 + fontHeight / 2, getWidth());
+
+	// draw OriantedAxes (sic!)
+	vec3 xAxis(1.f, 0.f, 0.f);
+	vec3 yAxis(0.f, 1.f, 0.f);
+	vec3 zAxis(0.f, 0.f, 1.f);
+	TimelineData& data = TimelineData::getTimelineData();
+	Interpolator& interpolator = data.getInterpolator();
+	quat rotation = data.getQuatFromValue(interpolator.getCurrentUniformState("camera_rotation").first);
+	xAxis = rotation * xAxis;
+	yAxis = rotation * yAxis;
+	zAxis = rotation * zAxis;
+	const float axesWidth = bounds.getHeight() * .1f;
+	const Point<float> centerPoint = bounds.getBottomLeft().translated(axesWidth * 1.5f, -axesWidth * 1.5f).toFloat();
+	const Point<float> xTransformed(xAxis.x * axesWidth, -xAxis.y * axesWidth);
+	const Point<float> yTransformed(yAxis.x * axesWidth, -yAxis.y * axesWidth);
+	const Point<float> zTransformed(zAxis.x * axesWidth, -zAxis.y * axesWidth);
+	const Point<float> xTip = centerPoint + xTransformed;
+	const Point<float> yTip = centerPoint + yTransformed;
+	const Point<float> zTip = centerPoint + zTransformed;
+	g.setOpacity(1.f);
+	g.setColour(Colours::red);
+	g.drawLine(Line<float>(centerPoint, xTip), 4.f);
+	const Point<float> xText = xTip + xTransformed * .1f;
+	g.drawSingleLineText("x", xText.getX(), xText.getY());
+	g.setColour(Colours::green);
+	const Point<float> yText = yTip + yTransformed * .1f;
+	g.drawLine(Line<float>(centerPoint, yTip), 4.f);
+	g.drawSingleLineText("y", yText.getX(), yText.getY());
+	g.setColour(Colours::blue);
+	const Point<float> zText = zTip + zTransformed * .1f;
+	g.drawLine(Line<float>(centerPoint, zTip), 4.f);
+	g.drawSingleLineText("z", zText.getX(), zText.getY());
 }
