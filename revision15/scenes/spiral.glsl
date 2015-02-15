667bde04f74d7481b2eed97d2316577454149a80
Vincent Schüßler <v.schuessler@gmail.com>
2015-02-15 12:05:45 +0100

    Add spiral scene to revision15

---
+++ revision15/scenes/spiral.glsl
@@ -0,0 +1,58 @@
+#version 430
+
+#include "rtificial.glsl"
+#line 5
+
+out vec4 out_color;
+
+void main() {
+	vec3 o = camera_position;
+	float screenDist;
+	vec3 d = get_direction(screenDist);
+	float t = march(o, d, 100., screenDist);
+
+	if (isinf(t)) {
+		out_color.rgb = vec3(0.);
+	} else {
+		out_color.rgb = vec3(max(dot(calc_normal(o + t * d, false), normalize(vec3(1., .5, 0.))), 0.) + .1);
+	}
+}
+
+vec2 f(vec3 p, bool last_step) {
+	//p.z += 10.;
+	//return vec2(sphere(p, 1.), 0.);
+	float d;
+	d = p.y;
+	vec3 p_grosse_dinger = p;
+	float alpha = atan(p_grosse_dinger.z, p_grosse_dinger.x);
+	float r2 = length(p_grosse_dinger.xz);
+	float r3 = r2;
+	float cell2 = r2 / 20.;
+	r2 = mod(r2, 20.);
+	p_grosse_dinger.xz = r2 * vec2(cos(alpha), sin(alpha));
+	float radius = 1. + cell2 * .2;
+	p_grosse_dinger.y -= .8 * radius;
+	vec3 p_loecher = p;
+	float c3 = TAU / (10. + cell2 * 10.);
+	alpha = mod(alpha, c3) - .5 * c3;
+	p_loecher.xz = r3 * vec2(cos(alpha), sin(alpha));
+	p_loecher.y -= .8 * radius;
+	d = smin(d, max(torus82(p_grosse_dinger, vec2(3., radius)), -(length(p_loecher.zy) - radius * .3)), .5);
+
+	vec3 p_kleine_dinger = p;
+	float r = length(p_kleine_dinger.xz);
+	float phi = atan(p_kleine_dinger.z, p_kleine_dinger.x);
+	float c = TAU / 30.;
+	float cell = floor(phi / c) / 30. * .5 + .5;
+	phi += pow(r, .4) * .4;
+	phi = mod(phi, c) - .5 * c;
+	p_kleine_dinger.xz = r * vec2(cos(phi), sin(phi));
+	float c2 = 3.;
+	p_kleine_dinger.x -= .5 * c2;
+	p_kleine_dinger.x = mod(p_kleine_dinger.x, c2) - .5 * c2;
+	//p_kleine_dinger.y -= cell * 5.;
+	p_kleine_dinger.yz *= rot2D(radians(20.));
+	p_kleine_dinger.xy *= rot2D(radians(-20.));
+	d = min(d, torus(p_kleine_dinger, vec2(.5, .1)));
+	return vec2(d, 0.);
+}
