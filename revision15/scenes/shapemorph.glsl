a1b0a91e777371f25dbc425385d1b7a0bf73cf8d
ro0mquy <git@ro0mquy.de>
2015-02-25 17:00:22 +0100

    add some strange objects to shapemorph file

--- revision15/scenes/shapemorph.glsl
+++ revision15/scenes/shapemorph.glsl
@@ -16,9 +16,81 @@ void main() {
 	}
 }
 
+float trishape(vec3 p) {
+	float height_hex = .3;
+	vec3 p_hex = p;
+	float r_hex = mix(1., .5, clamp(p_hex.y / height_hex, 0., 10.));
+	p_hex.y -= height_hex * .5;
+	float f_hex = triprism(p_hex.xzy, vec2(r_hex, height_hex * .5));
+
+	vec3 p_hex2 = p_hex;
+	p_hex2.xz *= rot2D(TAU / 3. / 2.);
+	float f_hex2 = triprism(p_hex2.xzy, vec2(.7 * r_hex, height_hex * .5));
+
+	float f = f_hex;
+	f = smin(f, f_hex2, morph_smooth_rt_float); // 0.7 is a good value
+	f *= .5;
+	return f;
+}
+
+float kantenklumpen(vec3 p) {
+	vec3 p_penta = p;
+	float f_penta = pentaprism(p_penta.xzy, vec2(1., 1.));
+
+	p_penta = quat_rotate(p_penta, morph_rot_rt_quat);
+	float f_penta2 = pentaprism(p_penta.xzy, vec2(1., 1.));
+
+	p_penta = quat_rotate(p_penta, morph_rot_rt_quat);
+	float f_penta3 = pentaprism(p_penta.xzy, vec2(1., 1.));
+
+	p_penta = quat_rotate(p_penta, morph_rot_rt_quat);
+	float f_penta4 = pentaprism(p_penta.xzy, vec2(1., 1.));
+
+	float f = f_penta;
+	f = smax(f, f_penta2, morph_smooth_rt_float); // 0.5 is a good value
+	f = smax(f, f_penta3, morph_smooth_rt_float);
+	f = smax(f, f_penta4, morph_smooth_rt_float);
+	return f;
+}
+
+float trillant(vec3 p) {
+	float height = .4;
+	float width = mix(3., 1.5, clamp(abs(p.y / (height * .5)), 0., 1.));
+
+	vec2 p_c1 = p.xz;
+	p_c1 -= width * vec2(0., 1./sqrt(3.));
+	float f_c1 = length(p_c1) - width;
+
+	vec2 p_c2 = p.xz;
+	p_c2 -= width * vec2(-.5, -.5/sqrt(3.));
+	float f_c2 = length(p_c2) - width;
+
+	vec2 p_c3 = p.xz;
+	p_c3 -= width * vec2(.5, -.5/sqrt(3.));
+	float f_c3 = length(p_c3) - width;
+
+	float f = f_c1;
+	f = max(f, f_c2);
+	f = max(f, f_c3);
+	f = max(f, abs(p.y) - height * .5);
+	f *= .3;
+
+	vec3 p_tri = p;
+	p_tri.xz *= rot2D(TAU / 3. / 3.);
+	float f_tri = triprism(p_tri.xzy, vec2(.7/3. * width, height * .5));
+
+	p_tri.xz *= rot2D(TAU / 3. / 3.);
+	float f_tri2 = triprism(p_tri.xzy, vec2(.7/3. * width, height * .5));
+	f_tri = min(f_tri, f_tri2);
+
+	f = mix(f_tri, f, morph_mix_rt_float); // 0.9 or 1.2 are nice shapes
+	return f;
+}
+
 vec2 f(vec3 p, bool last_step) {
 	float d;
 	float f;
+	/*
 	{
 		float f_kein = max(length(p.xy) - .5, abs(p.z) - 1.);
 		float f_ein = eineck(p, vec2(1.));
@@ -101,8 +173,27 @@ vec2 f(vec3 p, bool last_step) {
 			f = mix(f_ein, f_kein, morph_mix_rt_float - 5.);
 		}
 	}
-	d = smin(d, f, morph_smooth_rt_float);
-
+	d = smin(d, f, morph_smooth_rt_float); // 0.3 or -0.4
 	f = d;
+	// */
+
+	/*
+	float height_hex = .3;
+	vec3 p_hex = p;
+	float r_hex = mix(1., .5, clamp(p_hex.y / height_hex, 0., 10.));
+	p_hex.y -= height_hex * .5;
+	float f_hex = hexprism(p_hex.xzy, vec2(r_hex, height_hex * .5));
+
+	vec3 p_hex2 = p_hex;
+	p_hex2.xz *= rot2D(radians(30.));
+	float f_hex2 = hexprism(p_hex2.xzy, vec2(r_hex, height_hex * .5));
+
+	f = f_hex;
+	f = smax(f, f_hex2, morph_smooth_rt_float); // .3
+	f *= .5;
+	// */
+
+	f = trillant(p);
+
 	return vec2(f, 0.);
 }
