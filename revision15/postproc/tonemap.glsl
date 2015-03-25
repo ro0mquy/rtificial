c189d5e55286b286d45a0b1381be591c2b442dab
Vincent Schüßler <v.schuessler@gmail.com>
2015-03-25 23:38:08 +0100

    Avoid negative powers in color grading formula

--- revision15/postproc/tonemap.glsl
+++ revision15/postproc/tonemap.glsl
@@ -27,5 +27,5 @@ void main() {
 	out_color = texture2D(color, tc).rgb * exp2(post_tonemap_exposure + 1.);
 	out_color = tonemap(out_color)/tonemap(vec3(W));
 	out_color = clamp(out_color, 0., 1.);
-	out_color = pow(post_color_gain * 2. * (out_color + (2. * post_color_lift - 1.) * (1. - out_color)), 1./max(post_color_gamma * 2., 1e-6));
+	out_color = pow(max(vec3(0.), post_color_gain * 2. * (out_color + (2. * post_color_lift - 1.) * (1. - out_color))), 1./max(post_color_gamma * 2., 1e-6));
 }
