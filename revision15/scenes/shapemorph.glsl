e50321c989e2fbf630a1653d911edb2facbf669b
ro0mquy <git@ro0mquy.de>
2015-04-02 23:46:07 +0200

    add opal to shapemorph and fix lighting

--- revision15/scenes/shapemorph.glsl
+++ revision15/scenes/shapemorph.glsl
@@ -23,7 +23,7 @@ void main() {
 		vec3 reflection_dir = reflect(d, normal);
 		vec3 reflection_color = textureLod(environment, reflection_dir, 0.).rgb;
 
-		vec3 n2 = vec3(1.1, 1.104, 1.106);
+		vec3 n2 = vec3(1.1, 1.104, 1.106) * morph_reflectivness_rt_float;
 		vec3 refraction_dir_r = refract(d, normal, n2.x);
 		vec3 refraction_dir_g = refract(d, normal, n2.y);
 		vec3 refraction_dir_b = refract(d, normal, n2.z);
@@ -34,11 +34,8 @@ void main() {
 
 		vec3 r0 = (1. - n2) / (1. + n2);
 		r0 *= r0;
-		vec3 H_r = .5 * (refraction_dir_r - d);
-		vec3 H_g = .5 * (refraction_dir_g - d); // <3 mercury
-		vec3 H_b = .5 * (refraction_dir_b - d);
-		vec3 HdotV = -vec3(dot(H_r, d), dot(H_g, d), dot(H_b, d));
-		vec3 r = r0 + (1. - r0) * pow(1. - HdotV, vec3(5.));
+		float HdotV = -dot(normal, d);
+		vec3 r = r0 + (1. - r0) * pow(1. - HdotV, 5.);
 
 		//out_color.rgb = vec3(max(dot(normal, normalize(vec3(1., .5, .5))), 0.) + .1);
 
@@ -61,13 +58,13 @@ void main() {
 		out_color *= morph_rt_color;
 
 		vec3 col = morph_rt_color;
-		float rough = 0.3;
+		float rough = morph_rough_rt_float;
 		float metallic = 1.;
 		out_color.rgb = ambientColor(normal, -d, col, rough, metallic);
 		// ganz viel spucke!
 		//out_color.rgb *= .05;
 		//out_color.rgb += ao_factor * 2.;
-		out_color.rgb = mix(out_color.rgb, (ao_factor * 5. + .5) * morph_rt_color, r);
+		out_color.rgb = mix((ao_factor * 15. + 1.) * morph_rt_color, out_color.rgb, r);
 
 		//out_color.rgb = abs(normal);
 	}
@@ -192,6 +189,45 @@ float octahedronthingie(vec3 p) {
 	return f;
 }
 
+float opal(vec3 p) {
+	const float size = 1.;
+
+	vec3 p_sphere = p;
+	p_sphere.x /= morph_opal_stretch_x_rt_float;
+	p_sphere.y /= morph_opal_stretch_y_rt_float;
+	float f_sphere = sphere(p_sphere, size);
+	f_sphere *= min(1., min(morph_opal_stretch_x_rt_float, morph_opal_stretch_y_rt_float));
+
+	vec3 p_box1 = p;
+	p_box1.xz *= rot2D(TAU / 12.);
+	float f_box1 = box(p_box1, .5*vec3(morph_opal_stretch_x_rt_float, morph_opal_stretch_y_rt_float, size));
+
+	vec3 p_box2 = p;
+	p_box2.xz *= rot2D(-TAU / 12.);
+	float f_box2 = box(p_box2, .5*vec3(morph_opal_stretch_x_rt_float, morph_opal_stretch_y_rt_float, size));
+
+	float f = min(f_box1, f_box2);
+	f = mix(f_sphere, f, morph_opal_smooth_rt_float);
+
+
+	float smooth_plane = size * morph_opal_smooth_plane_rt_float;
+
+	float f_plane = abs(p.y);
+	f_plane = smax(f, f_plane, smooth_plane);
+
+	float f_plane1 = abs(p_box1.x);
+	f_plane1 = smax(f, f_plane1, smooth_plane);
+	f_plane = min(f_plane, f_plane1);
+
+	float f_plane2 = abs(p_box2.x);
+	f_plane2 = smax(f, f_plane2, smooth_plane);
+	f_plane = min(f_plane, f_plane2);
+
+	f = mix(f, f_plane, smooth_plane);
+
+	return f;
+}
+
 vec2 f(vec3 p, bool last_step) {
 	float d;
 	float f;
@@ -300,22 +336,24 @@ vec2 f(vec3 p, bool last_step) {
 
 	//*
 	if (morph_mix_rt_float <= 1.) {
-		f = mix(kristall(p), hexshape(p), morph_mix_rt_float);
+		f = mix(kristall(p), hexshape(p), smoothstep(0., 1., morph_mix_rt_float));
 	} else if (morph_mix_rt_float <= 2.) {
-		f = mix(hexshape(p), kantenklumpen(p), morph_mix_rt_float - 1.);
+		f = mix(hexshape(p), kantenklumpen(p), smoothstep(1., 2., morph_mix_rt_float));
 	} else if (morph_mix_rt_float <= 3.) {
-		f = mix(kantenklumpen(p), octahedronthingie(p), morph_mix_rt_float - 2.);
+		f = mix(kantenklumpen(p), octahedronthingie(p), smoothstep(2., 3., morph_mix_rt_float));
 	} else if (morph_mix_rt_float <= 4.) {
-		f = mix(octahedronthingie(p), trishape(p), morph_mix_rt_float - 3.);
+		f = mix(octahedronthingie(p), trishape(p), smoothstep(3., 4., morph_mix_rt_float));
 	} else if (morph_mix_rt_float <= 5.) {
-		f = mix(trishape(p), trillant(p), morph_mix_rt_float - 4.);
+		f = mix(trishape(p), trillant(p), smoothstep(4., 5., morph_mix_rt_float));
+	} else if (morph_mix_rt_float <= 6.) {
+		f = mix(trillant(p), opal(p), smoothstep(5., 6., morph_mix_rt_float));
 	}
 	// */
 
-	//f = octahedronthingie(p);
+	//f = opal(p);
 
 	vec2 m_kristall = vec2(f, 0.);
 	vec2 m_bg = vec2(background(p), 0.);
-	m_bg = min_material(m_bg, m_kristall);
-	return m_bg;
+	m_kristall = min_material(m_bg, m_kristall);
+	return m_kristall;
 }
