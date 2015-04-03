2d9070254c51927465ec86f86e47445437c75d0b
Vincent Schüßler <v.schuessler@gmail.com>
2015-04-03 17:11:14 +0200

    Modify mienenkraft fels color by height

--- revision15/scenes/mienenkraft.glsl
+++ revision15/scenes/mienenkraft.glsl
@@ -43,6 +43,7 @@ void main() {
 		if (material_id == fels_id || material_id == berg_id) {
 			rough = mk_fels_rough_rt_float;
 			float fels_noise = pow(cfbm(p * mk_fels_freq_rt_float) * .5 + .5, mk_fels_noise_power_rt_float);
+			fels_noise *= smoothstep(0., mk_kristall_h_rt_float, p.y * mk_height_color_rt_float);
 			out_color = mix(mk_fels_color1_rt_color, mk_fels_color2_rt_color, fels_noise);
 		} else if (material_id == kristall_id) {
 			out_color.b *= 0.2;
