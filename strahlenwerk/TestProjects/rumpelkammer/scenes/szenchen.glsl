082e93fd9fa04293222fc69a8c0858207870a023
Vincent Schüßler <v.schuessler@gmail.com>
2015-04-20 14:18:58 +0200

    Filter sdf visualization texture

--- strahlenwerk/TestProjects/rumpelkammer/scenes/szenchen.glsl
+++ strahlenwerk/TestProjects/rumpelkammer/scenes/szenchen.glsl
@@ -6,6 +6,31 @@ out vec4 out_color;
 
 float sdf(vec3);
 
+vec3 sampleDebugTexture(vec3 p) {
+	float dist = f(p, true);
+	float isolines = abs(sin(30. * dist));
+	return exp(-.1 * dist) * isolines * kk_color_rt_color;//vec3(.9, 1., .9);
+}
+
+vec3 sampleDebugTextureFiltered(vec3 p, vec3 pX, vec3 pY) {
+	//float detail = 100.;
+	//int MaxSamples = 10;
+	//int sx = 1 + clamp( int( detail*length(pX) ), 0, MaxSamples-1 );
+	//int sy = 1 + clamp( int( detail*length(pY) ), 0, MaxSamples-1 );
+	// fuck it - just supersample everything!
+	int sx = 5;
+	int sy = 5;
+
+	vec3 no = vec3(0);
+	for(int j = 0; j < sy; j++ ) {
+		for(int i = 0; i < sx; i++ ) {
+			vec2 st = (vec2(i, j) + .5)/vec2(sx, sy) - .5;
+			no += sampleDebugTexture(p + st.x * pX + st.y * pY);
+		}
+	}
+	return no / float(sx*sy);
+}
+
 void main() {
 	vec3 o = camera_position;
 	float screenDist;
@@ -23,9 +48,10 @@ void main() {
 		out_color.rgb = .5 * normal + .5;
 
 		if (material == 42.) {
-			float dist = f(hit, true);
-			dist = abs(sin(30. * dist));
-			out_color.rgb = exp(-.1 * t) * dist * kk_color_rt_color;//vec3(.9, 1., .9);
+			vec3 p = hit;
+			vec3 pX = dFdx(p);
+			vec3 pY = dFdy(p);
+			out_color.rgb = sampleDebugTextureFiltered(p, pX, pY);
 		}
 	}
 }
