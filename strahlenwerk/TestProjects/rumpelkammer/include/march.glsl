4b76e1691617854e362c7bdaa809b31d92a17c71
ro0mquy <git@ro0mquy.de>
2015-04-27 21:40:22 +0200

    add some basic union and intersection primitives

--- strahlenwerk/TestProjects/rumpelkammer/include/march.glsl
+++ strahlenwerk/TestProjects/rumpelkammer/include/march.glsl
@@ -598,11 +598,85 @@ float pMirrorAtPlane(inout vec3 p, vec3 planeNormal, float offset) {
 	return sgn(dist);
 }
 
+// unions two objects and adds rounded edges with radius r
+// use only with orthogonal objects
+float opUnionRounded(float f1, float f2, float r) {
+	float f_min = min(f1, f2);
+	if (f1 < r && f2 < r) {
+		vec2 q = vec2(f1, f2);
+		vec2 center = vec2(r);
+		float f_round = r - distance(center, q); // inside of fSphere2
+		return min(f_round, f_min);
+	}
+	return f_min;
+}
+
+// intersects two objects and adds rounded edges with radius r
+// use only with orthogonal objects
+float opIntersectRounded(float f1, float f2, float r) {
+	float f_max = max(f1, f2);
+	if (f1 > -r && f2 > -r) {
+		vec2 q = vec2(f1, f2);
+		vec2 center = vec2(-r);
+		float f_round = distance(center, q) - r;
+		return max(f_round, f_max);
+	}
+	return f_max;
+}
+
+// subtracts f2 from f1 and adds rounded edges with radius r
+// use only with orthogonal objects
+float opSubtractRounded(float f1, float f2, float r) {
+	return opIntersectRounded(f1, -f2, r);
+}
+
+// unions two objects and adds some kind of chamfer with radius r
+// use only with orthogonal objects
+float opUnionChamfer(float f1, float f2, float r) {
+	float f_min = min(f1, f2);
+	//if (f1 < r && f2 < r) {
+		float f_chamfer = sqrt(.5) * (f1 + f2 - r);
+		return min(f_chamfer, f_min);
+	//}
+	//return f_min;
+}
+
+// intersects two objects and adds some kind of chamfer with radius r
+// use only with orthogonal objects
+float opIntersectChamfer(float f1, float f2, float r) {
+	float f_max = max(f1, f2);
+	//if (f1 > -r && f2 > -r) {
+		float f_chamfer = sqrt(.5) * (f1 + f2 + r);
+		return max(f_chamfer, f_max);
+	//}
+	//return f_max;
+}
+
+// subtracts f2 from f1 and adds some kind of chamfer with radius r
+// use only with orthogonal objects
+float opSubtractChamfer(float f1, float f2, float r) {
+	return opIntersectChamfer(f1, -f2, r);
+}
+
+// like normal min()-union but with correct distance at corners
+// gives correct interior
+// use only with orthogonal objects
+float opUnionEuclid(float f1, float f2) {
+	vec2 q = vec2(f1, f2);
+	return max(minV(q), 0.) - length(min(q, 0.));
+}
+
 // like normal max()-intersection but with correct distance at corners
 // use only with orthogonal objects
 float opIntersectEuclid(float f1, float f2) {
 	vec2 q = vec2(f1, f2);
 	return min(maxV(q), 0.) + length(max(q, 0.));
+}
+
+// like normal max()-subtraction but with correct distance at corners
+// use only with orthogonal objects
+float opSubtractEuclid(float f1, float f2) {
+	return opIntersectEuclid(f1, -f2);
 }
 
 float fSphere(vec3 p, float r) {
