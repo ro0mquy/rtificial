eafda45d23473888f62f5278869dee9e617a67e6
ro0mquy <git@ro0mquy.de>
2015-04-30 23:14:06 +0200

    add smoothed op*Chamfer

--- strahlenwerk/TestProjects/rumpelkammer/include/march.glsl
+++ strahlenwerk/TestProjects/rumpelkammer/include/march.glsl
@@ -598,6 +598,55 @@ float pMirrorAtPlane(inout vec3 p, vec3 planeNormal, float offset) {
 	return sgn(dist);
 }
 
+// unions two object and produces a very smooth transition
+// affects an area of r between the meet point, underestimates the distance
+// can be used with any kind of objects
+float opUnionSmooth(float f1, float f2, float r) {
+	// maybe remove this min() and if() stuff
+	//float f_min = min(f1, f2);
+	if (f1 < r && f2 < r) {
+		float h = clamp(.5 + .5 * (f2 - f1) / r, 0., 1. );
+		float f_smooth = mix(f2, f1, h) - r * h * (1. - h);
+		return f_smooth;
+		//return min(f_smooth, f_min);
+	}
+	float f_min = min(f1, f2);
+	return f_min;
+}
+
+// intersects two object and produces a very smooth transition
+// affects an area of r between the meet point, underestimates the distance
+// can be used with any kind of objects
+float opIntersectSmooth(float f1, float f2, float r) {
+	// maybe remove this max() and if() stuff
+	//float f_max = max(f1, f2);
+	//if (f1 > -r && f2 > -r) {
+	if (abs(f1) < r && abs(f2) < r) {
+		float h = clamp(.5 - .5 * (f2 - f1) / r, 0., 1. );
+		float f_smooth = mix(f2, f1, h) + r * h * (1. - h);
+		return f_smooth;
+		//return max(f_smooth, f_max);
+	}
+	float f_max = max(f1, f2);
+	return f_max;
+}
+
+// subtracts f2 from f1 and produces a very smooth transition
+// affects an area of r between the meet point, underestimates the distance
+// can be used with any kind of objects
+float opSubtractSmooth(float f1, float f2, float r) {
+	return opIntersectSmooth(f1, -f2, r);
+}
+
+// all hail the smin
+float smin(float f1, float f2, float r) {
+	return opUnionSmooth(f1, f2, r);
+}
+
+float smax(float f1, float f2, float r) {
+	return opIntersectSmooth(f1, f2, r);
+}
+
 // unions two objects and adds rounded edges with radius r
 // use only with orthogonal objects
 float opUnionRounded(float f1, float f2, float r) {
@@ -658,6 +707,34 @@ float opSubtractChamfer(float f1, float f2, float r) {
 	return opIntersectChamfer(f1, -f2, r);
 }
 
+// unions two objects and adds some kind of smooth chamfer with radius r
+// use only with orthogonal objects
+float opUnionChamferSmooth(float f1, float f2, float r) {
+	float f_min = min(f1, f2);
+	//if (f1 < r && f2 < r) {
+		float f_chamfer = sqrt(.5) * (f1 + f2 - r);
+		return opUnionSmooth(f_chamfer, f_min, .1 * r);
+	//}
+	//return f_min;
+}
+
+// intersects two objects and adds some kind of chamfer with radius r
+// use only with orthogonal objects
+float opIntersectChamferSmooth(float f1, float f2, float r) {
+	float f_max = max(f1, f2);
+	//if (f1 > -r && f2 > -r) {
+		float f_chamfer = sqrt(.5) * (f1 + f2 + r);
+		return opIntersectSmooth(f_chamfer, f_max, .1 * r);
+	//}
+	//return f_max;
+}
+
+// subtracts f2 from f1 and adds some kind of chamfer with radius r
+// use only with orthogonal objects
+float opSubtractChamferSmooth(float f1, float f2, float r) {
+	return opIntersectChamferSmooth(f1, -f2, r);
+}
+
 // unions two objects and adds n columns within a radius of r
 // use only with orthogonal objects
 float opUnionColumns(float f1, float f2, float r, float n) {
@@ -703,55 +780,6 @@ float opIntersectColumns(float f1, float f2, float r, float n) {
 // use only with orthogonal objects
 float opSubtractColumns(float f1, float f2, float r, float n) {
 	return opIntersectColumns(f1, -f2, r, n);
-}
-
-// unions two object and produces a very smooth transition
-// affects an area of r between the meet point, underestimates the distance
-// can be used with any kind of objects
-float opUnionSmooth(float f1, float f2, float r) {
-	// maybe remove this min() and if() stuff
-	//float f_min = min(f1, f2);
-	if (f1 < r && f2 < r) {
-		float h = clamp(.5 + .5 * (f2 - f1) / r, 0., 1. );
-		float f_smooth = mix(f2, f1, h) - r * h * (1. - h);
-		return f_smooth;
-		//return min(f_smooth, f_min);
-	}
-	float f_min = min(f1, f2);
-	return f_min;
-}
-
-// intersects two object and produces a very smooth transition
-// affects an area of r between the meet point, underestimates the distance
-// can be used with any kind of objects
-float opIntersectSmooth(float f1, float f2, float r) {
-	// maybe remove this max() and if() stuff
-	//float f_max = max(f1, f2);
-	//if (f1 > -r && f2 > -r) {
-	if (abs(f1) < r && abs(f2) < r) {
-		float h = clamp(.5 - .5 * (f2 - f1) / r, 0., 1. );
-		float f_smooth = mix(f2, f1, h) + r * h * (1. - h);
-		return f_smooth;
-		//return max(f_smooth, f_max);
-	}
-	float f_max = max(f1, f2);
-	return f_max;
-}
-
-// subtracts f2 from f1 and produces a very smooth transition
-// affects an area of r between the meet point, underestimates the distance
-// can be used with any kind of objects
-float opSubtractSmooth(float f1, float f2, float r) {
-	return opIntersectSmooth(f1, -f2, r);
-}
-
-// all hail the smin
-float smin(float f1, float f2, float r) {
-	return opUnionSmooth(f1, f2, r);
-}
-
-float smax(float f1, float f2, float r) {
-	return opIntersectSmooth(f1, f2, r);
 }
 
 // like normal min()-union but with correct distance at corners
