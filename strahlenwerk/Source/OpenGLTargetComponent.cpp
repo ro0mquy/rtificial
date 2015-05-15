443f5d4575455da08b6bd7ae3633787c710b4d07
ro0mquy <git@ro0mquy.de>
2015-05-15 23:55:13 +0200

    fix camera rotation pivot

--- strahlenwerk/Source/OpenGLTargetComponent.cpp
+++ strahlenwerk/Source/OpenGLTargetComponent.cpp
@@ -50,40 +50,52 @@ void OpenGLTargetComponent::paint(Graphics& g) {
 	g.drawMultiLineText(frameTimeString, 10, 12 + fontHeight / 2, getWidth());
 
 	// draw OriantedAxes (sic!)
+	TimelineData& data = TimelineData::getTimelineData();
+	Interpolator& interpolator = data.getInterpolator();
+
+	ValueTree defaultRotation(treeId::interpolatedValue);
+	data.initializeValue(defaultRotation, "quat");
+
+	quat rotation = data.getQuatFromValue(interpolator.getCurrentUniformState("camera_rotation", defaultRotation).first);
+	// display world rotation, not camera rotation
+	rotation = conjugate(rotation);
+
 	vec3 xAxis(1.f, 0.f, 0.f);
 	vec3 yAxis(0.f, 1.f, 0.f);
 	vec3 zAxis(0.f, 0.f, 1.f);
-	TimelineData& data = TimelineData::getTimelineData();
-	Interpolator& interpolator = data.getInterpolator();
-	ValueTree defaultRotation(treeId::interpolatedValue);
-	data.initializeValue(defaultRotation, "quat");
-	quat rotation = data.getQuatFromValue(interpolator.getCurrentUniformState("camera_rotation", defaultRotation).first);
 	xAxis = rotation * xAxis;
 	yAxis = rotation * yAxis;
 	zAxis = rotation * zAxis;
+
 	const float axesWidth = bounds.getHeight() * .1f;
 	const Point<float> centerPoint = bounds.getBottomLeft().translated(axesWidth * 1.5f, -axesWidth * 1.5f).toFloat();
+
 	const Point<float> xTransformed(xAxis.x * axesWidth, -xAxis.y * axesWidth);
 	const Point<float> yTransformed(yAxis.x * axesWidth, -yAxis.y * axesWidth);
 	const Point<float> zTransformed(zAxis.x * axesWidth, -zAxis.y * axesWidth);
+
 	const Point<float> xTip = centerPoint + xTransformed;
 	const Point<float> yTip = centerPoint + yTransformed;
 	const Point<float> zTip = centerPoint + zTransformed;
+
 	g.setOpacity(1.f);
-	//g.setColour(Colour(0xffff0000));
 	// "Hinten dunkel vorne hell" "Ja, das ginge doch!"
 	// Das ist zwar jetzt nicht schön hier und eigentlich immernoch total unmöglich zu erkennen, aber es ging schnell und tut!
+
+	//g.setColour(Colour(0xffff0000));
 	g.setGradientFill(ColourGradient(Colour(0xffff0000), centerPoint.x, centerPoint.y,
 				xAxis.z < 0.f ? Colour(0xffff0000).darker(-xAxis.z) : Colour(0xffff0000).brighter(xAxis.z), xTip.x, xTip.y, false));
 	g.drawLine(Line<float>(centerPoint, xTip), 4.f);
 	const Point<float> xText = xTip + xTransformed * .1f;
 	g.drawSingleLineText("x", xText.getX(), xText.getY());
+
 	//g.setColour(Colour(0xff00ff00));
 	g.setGradientFill(ColourGradient(Colour(0xff00ff00), centerPoint.x, centerPoint.y,
 				yAxis.z < 0.f ? Colour(0xff00ff00).darker(-yAxis.z) : Colour(0xff00ff00).brighter(yAxis.z), yTip.x, yTip.y, false));
 	const Point<float> yText = yTip + yTransformed * .1f;
 	g.drawLine(Line<float>(centerPoint, yTip), 4.f);
 	g.drawSingleLineText("y", yText.getX(), yText.getY());
+
 	//g.setColour(Colour(0xff0000ff));
 	g.setGradientFill(ColourGradient(Colour(0xff0000ff), centerPoint.x, centerPoint.y,
 				zAxis.z < 0.f ? Colour(0xff0000ff).darker(-zAxis.z) : Colour(0xff0000ff).brighter(zAxis.z), zTip.x, zTip.y, false));
