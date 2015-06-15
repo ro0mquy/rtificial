37138ac665eb5a952ea384b110612ba9daa75cef
Vincent Schüßler <v.schuessler@gmail.com>
2015-06-16 00:09:11 +0200

    Add tunnel scene

---
+++ geballer/scenes/tunnel.glsl
@@ -0,0 +1,56 @@
+#include "march.glsl"
+#include "noise.glsl"
+#line 4 "rotator"
+
+float fSupport(vec3 p, float radius) {
+	float rBig = radius;
+	float rSmall = tunnel_support_radius_rt_float * rBig;
+	rBig -= rSmall;
+	vec2 q = vec2(f2Hexprism(p.xy, rBig), p.z);
+	vec2 q_cutout = q;
+	pRot(q, Tau / 6 * .5);
+	float d_torus = f2Hexprism(q, rSmall);
+	float width = tunnel_support_cutout_width_rt_float * rSmall;
+	float depth = tunnel_support_cutout_depth_rt_float * rSmall;
+	pDomrepAngle(q_cutout, 6, rSmall - depth);
+	float d_cutout = f2Box(q_cutout, vec2(depth, width - depth));
+	d_torus = opSubtractStairs(d_torus, d_cutout, depth, 6);
+	return d_torus;
+}
+
+float fScene(vec3 p) {
+	float d = fSphere(p, 1);
+
+	float tunnel_radius = tunnel_radius_rt_float;
+	vec3 p_tunnel = p;
+	float tunnel_height = tunnel_radius * (Golden_Ratio - 1.);
+	pCutAndExtrude(p_tunnel, vec3(0, -tunnel_height * .5, 0), vec3(0, tunnel_height, 0));
+	pTrans(p_tunnel.x, -tunnel_height);
+	pCutAndExtrude(p_tunnel, vec3(0), vec3(tunnel_height * 2, 0, 0));
+	vec3 v = vec3(0, tunnel_height, 0);
+	float distance_between_support = tunnel_support_spacing_rt_float * tunnel_radius;
+	vec3 p_support = p_tunnel;
+	float d_tunnel = -f2Hexprism(p_tunnel.xy, tunnel_radius);
+	d = d_tunnel;
+
+	pDomrep(p_support.z, distance_between_support);
+	float d_support = fSupport(p_support, tunnel_radius);
+	d = min(d, d_support);
+
+	mUnion(d, MaterialId(0, p));
+	return d;
+}
+
+vec3 applyLights(vec3 origin, float marched, vec3 direction, vec3 hit, vec3 normal, MaterialId materialId, Material material) {
+	return applyNormalLights(origin, marched, direction, hit, normal, material);
+}
+
+vec3 applyAfterEffects(vec3 origin, float marched, vec3 direction, vec3 color) {
+	return color;
+}
+
+Material getMaterial(MaterialId materialId) {
+	Material mat = defaultMaterial(vec3(1));
+	mat.roughness = .1;
+	return mat;
+}
