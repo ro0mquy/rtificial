4c5cdafa3ac9d4ecb82fd0b056643d6583dc9fe0
Vincent Schüßler <v.schuessler@gmail.com>
2015-06-25 13:24:42 +0200

    Add scenes for unten

---
+++ geballer/scenes/karpador.glsl
@@ -0,0 +1,23 @@
+#include "march.glsl"
+#line 3
+
+float fScene(vec3 p) {
+	float f = fSphere(p, 1.);
+
+	mUnion(f, MaterialId(0., p));
+	return f;
+}
+
+vec3 applyLights(vec3 origin, float marched, vec3 direction, vec3 hit, vec3 normal, MaterialId materialId, Material material) {
+	return applyNormalLights(origin, marched, direction, hit, normal, material);
+}
+
+vec3 applyAfterEffects(vec3 origin, float marched, vec3 direction, vec3 color) {
+	return color;
+}
+
+Material getMaterial(MaterialId materialId) {
+	Material mat = defaultMaterial(vec3(1));
+	mat.roughness = .1;
+	return mat;
+}
