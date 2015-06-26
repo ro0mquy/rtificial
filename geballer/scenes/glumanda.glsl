78b225e9ef2e1aad007fd52cab8643acd301db09
Vincent Schüßler <v.schuessler@gmail.com>
2015-06-26 12:28:48 +0200

    Add parameters and bounding object to spimi

--- geballer/scenes/glumanda.glsl
+++ geballer/scenes/glumanda.glsl
@@ -1,34 +1,44 @@
 #include "march.glsl"
 #line 3
 
-float fSpimiDisc(vec3 p, float i, float discAtan) {
-	float innerRadius = 2 + sin(i) * .5;
+float fSpimiDisc(vec3 p, float scale, float i, float discAtan) {
+	float innerRadius = glum_spimi_radius_rt_float + sin(i) * glum_spimi_radius_var_rt_float;
+	innerRadius *= scale;
+	// TODO parameterize
 	float rotationAngle = Tau / 10 * i / (5 + 3.5 * sin(time));
 	pRotY(p, rotationAngle);
-	pDomrepAngleWithAtan(p.xz, 10, innerRadius, rotationAngle + discAtan);
-	float d = fBox(p, vec3(.3, .3, 1));
-	pTrans(p.x, .5 + .3);
-	float d2 = fConeCapped(p.zxy, .3, .08, 1);
-	return opUnionChamfer(d, d2, .05);
+	pDomrepAngleWithAtan(p.xz, glum_spimi_n_spikes_rt_float, innerRadius, rotationAngle + discAtan);
+	float disc_height = glum_spimi_disc_height_rt_float * scale;
+	float d = fBox(p, vec3(disc_height, disc_height, scale));
+	pTrans(p.x, disc_height + glum_spimi_spike_length_rt_float * scale);
+	float d2 = fConeCapped(p.zxy, disc_height - .05 * scale, disc_height * glum_spimi_spike_cap_rt_float, glum_spimi_spike_length_rt_float * scale);
+	return opUnionChamfer(d, d2, .05 * scale);
 }
 
 float fSpimi(vec3 p, float scale) {
-	float c = .6;
+	float maxSpimiRadius = glum_spimi_radius_rt_float + glum_spimi_radius_var_rt_float;
+	maxSpimiRadius += glum_spimi_disc_height_rt_float + glum_spimi_spike_length_rt_float * 2;
+	maxSpimiRadius *= scale;
+	float f_bounding = f2Sphere(p.xz, maxSpimiRadius);
+	if (f_bounding > 1) {
+		return f_bounding;
+	}
+	float c = 2 * glum_spimi_disc_height_rt_float * scale;
 	float i = pDomrep(p.y, c);
 	//float i = 0;
 	float discAtan = atan(p.z, p.x);
 	// TODO use simpler objects as guards
 	pTrans(p.y, c);
-	float d1 = fSpimiDisc(p, i + 1, discAtan);
+	float d1 = fSpimiDisc(p, scale, i + 1, discAtan);
 	pTrans(p.y, -c);
-	float d2 = fSpimiDisc(p, i, discAtan);
+	float d2 = fSpimiDisc(p, scale, i, discAtan);
 	pTrans(p.y, -c);
-	float d3 = fSpimiDisc(p, i - 1, discAtan);
+	float d3 = fSpimiDisc(p, scale, i - 1, discAtan);
 	return min(d1, min(d2, d3));
 }
 
 float fScene(vec3 p) {
-	float f = fSpimi(p, 1.);
+	float f = fSpimi(p, 1);
 
 	mUnion(f, MaterialId(0., p));
 	return f;
