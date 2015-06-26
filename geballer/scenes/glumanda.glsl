985116d89506f4638db7651ad5507bdddea84bd1
Vincent Schüßler <v.schuessler@gmail.com>
2015-06-26 11:23:33 +0200

    Copy spimi to glumanda

--- geballer/scenes/glumanda.glsl
+++ geballer/scenes/glumanda.glsl
@@ -1,8 +1,34 @@
 #include "march.glsl"
 #line 3
 
+float fSpimiDisc(vec3 p, float i, float discAtan) {
+	float innerRadius = 2 + sin(i) * .5;
+	float rotationAngle = Tau / 10 * i / (5 + 3.5 * sin(time));
+	pRotY(p, rotationAngle);
+	pDomrepAngleWithAtan(p.xz, 10, innerRadius, rotationAngle + discAtan);
+	float d = fBox(p, vec3(.3, .3, 1));
+	pTrans(p.x, .5 + .3);
+	float d2 = fConeCapped(p.zxy, .3, .08, 1);
+	return opUnionChamfer(d, d2, .05);
+}
+
+float fSpimi(vec3 p, float scale) {
+	float c = .6;
+	float i = pDomrep(p.y, c);
+	//float i = 0;
+	float discAtan = atan(p.z, p.x);
+	// TODO use simpler objects as guards
+	pTrans(p.y, c);
+	float d1 = fSpimiDisc(p, i + 1, discAtan);
+	pTrans(p.y, -c);
+	float d2 = fSpimiDisc(p, i, discAtan);
+	pTrans(p.y, -c);
+	float d3 = fSpimiDisc(p, i - 1, discAtan);
+	return min(d1, min(d2, d3));
+}
+
 float fScene(vec3 p) {
-	float f = fSphere(p, 1.);
+	float f = fSpimi(p, 1.);
 
 	mUnion(f, MaterialId(0., p));
 	return f;
