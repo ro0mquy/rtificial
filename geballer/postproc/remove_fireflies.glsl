ed68bf50f6916d6f3c9b843bd22db95ece1b01bf
Vincent Schüßler <v.schuessler@gmail.com>
2015-07-29 12:03:12 +0200

    Add 'remove fireflies' postprocessing step

--- geballer/postproc/remove_fireflies.glsl
+++ geballer/postproc/remove_fireflies.glsl
@@ -3,10 +3,10 @@
 #include "median.glsl"
 #line 5
 
-uniform sampler2D color; // vec4
-out vec4 out_color;
+uniform sampler2D color; // vec3
+out vec3 out_color;
 
-// level(1)
+uniform bool post_disable_median;
 
 float lumaaaaa(vec3 color) {
 	return dot(color, vec3(.2126, .7152, .0722));
@@ -17,21 +17,21 @@ void main() {
 	vec2 base = tc - pixelSize;
 	vec3 colors[9];
 	float values[9];
-	float alphas[9];
 	for (int y = 0; y < 3; y++) {
 		for (int x = 0; x < 3; x++) {
-			vec4 sampleValue = textureLod(color, base + vec2(x, y) * pixelSize, 0);
-			colors[y * 3 + x] = sampleValue.rgb;
+			vec3 sampleValue = textureLod(color, base + vec2(x, y) * pixelSize, 0).rgb;
+			colors[y * 3 + x] = sampleValue;
 			values[y * 3 + x] = lumaaaaa(colors[y * 3 + x]);
-			alphas[y * 3 + x] = sampleValue.a;
 		}
 	}
 
 	float median_luma = median(values);
-	out_color.a = median(alphas);
 	for (int i = 0; i < 9; i++) {
 		if ((values[i] - median_luma) < 1e-6) {
-			out_color.rgb = colors[i];
+			out_color = colors[i];
 		}
+	}
+	if (post_disable_median) {
+		out_color = colors[5];
 	}
 }
