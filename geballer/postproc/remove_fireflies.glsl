99550c83c5a5ddc5f4d88c9ed5716c5db2d09183
Vincent Schüßler <v.schuessler@gmail.com>
2015-07-29 13:25:27 +0200

    Improve 'remove fireflies' pass

--- geballer/postproc/remove_fireflies.glsl
+++ geballer/postproc/remove_fireflies.glsl
@@ -17,12 +17,19 @@ void main() {
 	vec2 base = tc - pixelSize;
 	vec3 colors[9];
 	float values[9];
+	float avg_value = 0;
 	for (int y = 0; y < 3; y++) {
 		for (int x = 0; x < 3; x++) {
 			vec3 sampleValue = textureLod(color, base + vec2(x, y) * pixelSize, 0).rgb;
 			colors[y * 3 + x] = sampleValue;
 			values[y * 3 + x] = lumaaaaa(colors[y * 3 + x]);
+			avg_value += values[y * 3 + x] * (1./9);
 		}
+	}
+
+	if (values[5] < avg_value * 2) {
+		out_color = colors[5];
+		return;
 	}
 
 	float median_luma = median(values);
