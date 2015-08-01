e046bd8aef982f5f6875ba92cf502f39ecaaa86b
ro0mquy <git@ro0mquy.de>
2015-08-01 23:13:55 +0200

    Revert "fix sequnces in runtime"
    This reverts commit 7f1b92fd9c88ab7fb54c2c2ede80e68d33122a27.

--- strahlenwerk_runtime/src/DataInterpolator.h
+++ strahlenwerk_runtime/src/DataInterpolator.h
@@ -9,8 +9,8 @@ namespace DataInterpolator {
 	void loadUniforms(const int time);
 	void setUniformValue(const int time, const int nthUniform, const int type, const int location);
 	void setValue(const int nthUniform, const int type, const int location, const int offset);
-	void setLinearValue(const int nthUniform, const int type, const int location, const int keyframeDataOffset, const int indexP1, const int indexP2, const float mixT, const int numKeyframes, const int useStdAtStart, const int useStdAtEnd);
-	void setSplineValue(const int nthUniform, const int type, const int location, const int keyframeDataOffset, const int indexP0, const int indexP1, const int indexP2, const int indexP3, const float mixT, const int numKeyframes, const int useStdAtStart, const int useStdAtEnd);
+	void setLinearValue(const int nthUniform, const int type, const int location, const int offset, const float mixT);
+	void setSplineValue(const int nthUniform, const int type, const int location, const int offset, const float mixT, const bool noFirstValue, const bool noLastValue);
 };
 
 #endif // DATAINTERPOLATOR_H
