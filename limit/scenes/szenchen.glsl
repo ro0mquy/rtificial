fb95e47608885d17b115c4a82a4916644855e207
ro0mquy <git@ro0mquy.de>
2015-08-04 19:40:59 +0200

    finished still standing man

--- limit/scenes/szenchen.glsl
+++ limit/scenes/szenchen.glsl
@@ -4,26 +4,99 @@
 const float id_floor = 0.;
 const float id_aman = 1.;
 
+float fAmanBox(vec3 p, vec2 pos, vec2 dim) {
+	float r = aman_cube_r_rt_float;
+	float d = 2. * r;
+
+	vec3 dimension = vec3(dim, 1) * r;
+	p.xy -= pos * d;
+	p -= dimension;
+	return fBox(p, dimension);
+}
+
 float fAman(vec3 p) {
-	float r_cube = aman_cube_r_rt_float;
-	float d_space = 2. * r_cube;
-	p.y -= r_cube;
+	float r = aman_cube_r_rt_float;
+	float d = 2. * r;
+	p.y -= r;
 
-	float f_foot_left1 = fBox(p, r_cube);
-	p.x -= d_space;
-	float f_foot_left2 = fBox(p, r_cube);
-	p.x -= d_space;
-	float f_foot_left3 = fBox(p, r_cube);
-	p.x -= d_space;
-	float f_foot_left4 = fBox(p, r_cube);
-	p.x -= d_space;
-	float f_foot_left5 = fBox(p, r_cube);
-	p.x -= d_space;
+	float f_foot_row = fAmanBox(p, vec2(0, 0), vec2(12, 1));
+	float f_foot_left = fAmanBox(p, vec2(0, 1), vec2(1, 1));
+	float f_foot_left_top = fAmanBox(p, vec2(1, 2), vec2(3, 1));
+	float f_foot_right = fAmanBox(p, vec2(5, 1), vec2(1, 1));
 
-	float f_foot_left = min4(f_foot_left1, f_foot_left2, f_foot_left3, f_foot_left4);
-	f_foot_left = min(f_foot_left, f_foot_left5);
+	float f_leg_left = fAmanBox(p, vec2(2, 3), vec2(1, 3));
+	float f_leg_middle = fAmanBox(p, vec2(6, 2), vec2(1, 3));
+	float f_leg_right = fAmanBox(p, vec2(11, 1), vec2(1, 3));
+	float f_leg_right_block1 = fAmanBox(p, vec2(10, 1), vec2(1, 1));
+	float f_leg_right_block2 = fAmanBox(p, vec2(10, 3), vec2(1, 1));
 
-	return f_foot_left;
+	float f_body_middle = fAmanBox(p, vec2(-2, 6), vec2(15, 1));
+	float f_body_left = fAmanBox(p, vec2(1, 7), vec2(1, 3));
+	float f_body_right = fAmanBox(p, vec2(9, 7), vec2(1, 3));
+
+	float f_hand_left = fAmanBox(p, vec2(0, 4), vec2(2, 1));
+	float f_hand_left_thumb = fAmanBox(p, vec2(-1, 5), vec2(1, 1));
+	float f_hand_right = fAmanBox(p, vec2(11, 4), vec2(2, 1));
+	float f_hand_right_thumb = fAmanBox(p, vec2(10, 5), vec2(1, 1));
+
+	float f_arm_left = fAmanBox(p, vec2(-2, 7), vec2(1, 4));
+	float f_arm_left_block = fAmanBox(p, vec2(0, 8), vec2(1, 1));
+	float f_arm_right = fAmanBox(p, vec2(13, 4), vec2(1, 7));
+	float f_arm_right_block = fAmanBox(p, vec2(12, 8), vec2(1, 1));
+
+	float f_shoulder_left = fAmanBox(p, vec2(0, 12), vec2(2, 1));
+	float f_shoulder_left_block = fAmanBox(p, vec2(-1, 11), vec2(1, 1));
+	float f_shoulder_right = fAmanBox(p, vec2(10, 12), vec2(2, 1));
+	float f_shoulder_right_block = fAmanBox(p, vec2(12, 11), vec2(1, 1));
+
+	float f_head_left = fAmanBox(p, vec2(2, 12), vec2(1, 3));
+	float f_head_right = fAmanBox(p, vec2(9, 12), vec2(1, 3));
+	float f_head_top = fAmanBox(p, vec2(3, 15), vec2(6, 1));
+	float f_head_bottom = fAmanBox(p, vec2(3, 11), vec2(6, 1));
+
+	float f_eye_left = fAmanBox(p, vec2(4, 13), vec2(1, 1));
+	float f_eye_right = fAmanBox(p, vec2(6, 13), vec2(1, 1));
+
+
+	float f = f_foot_row;
+	f = min(f, f_foot_left);
+	f = min(f, f_foot_left_top);
+	f = min(f, f_foot_right);
+
+	f = min(f, f_leg_left);
+	f = min(f, f_leg_middle);
+	f = min(f, f_leg_right);
+	f = min(f, f_leg_right_block1);
+	f = min(f, f_leg_right_block2);
+
+	f = min(f, f_body_middle);
+	f = min(f, f_body_left);
+	f = min(f, f_body_right);
+
+	f = min(f, f_hand_left);
+	f = min(f, f_hand_left_thumb);
+	f = min(f, f_hand_right);
+	f = min(f, f_hand_right_thumb);
+
+	f = min(f, f_arm_left);
+	f = min(f, f_arm_left_block);
+	f = min(f, f_arm_right);
+	f = min(f, f_arm_right_block);
+
+	f = min(f, f_shoulder_left);
+	f = min(f, f_shoulder_left_block);
+	f = min(f, f_shoulder_right);
+	f = min(f, f_shoulder_right_block);
+
+	f = min(f, f_head_left);
+	f = min(f, f_head_right);
+	f = min(f, f_head_top);
+	f = min(f, f_head_bottom);
+
+	f = min(f, f_eye_left);
+	f = min(f, f_eye_right);
+
+	return f;
 }
 
 float fScene(vec3 p) {
