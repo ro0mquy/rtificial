95b0cdc37de07cca032cb4fac3f9df71b3c8238f
Ps0ke <mail@ps0ke.de>
2015-09-28 15:24:57 +0200

    Make environment map size smaller, for our poor gpus.

--- vinyl/Source/AmbientLight.cpp
+++ vinyl/Source/AmbientLight.cpp
@@ -8,13 +8,17 @@ AmbientLight::AmbientLight(const char* source_) :
 {}
 
 void AmbientLight::create(Shader& diffuseShader, Shader& specularShader) {
+	const int env_size = 256;
+	const int diffuse_size = env_size / 4;
+	const int specular_size = env_size;
+
 	Shader shader(source, 0, nullptr);
 	shader.compile();
-	environment.create(1024, 1024, 1, shader);
+	environment.create(env_size, env_size, 1, shader);
 	shader.destroy();
 	environment.bind();
-	filteredDiffuse.create(256, 256, 1, diffuseShader);
-	filteredSpecular.create(1024, 1024, 6, specularShader);
+	filteredDiffuse.create(diffuse_size, diffuse_size, 1, diffuseShader);
+	filteredSpecular.create(specular_size, specular_size, 6, specularShader);
 }
 
 void AmbientLight::bind() {
