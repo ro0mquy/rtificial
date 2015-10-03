7031ec7d16f10a7554b62c5e1cea8eed78bd9150
ro0mquy <git@ro0mquy.de>
2015-10-03 14:09:17 +0200

    make hustensaft color customizable

--- limit/postproc/colorseparation.glsl
+++ limit/postproc/colorseparation.glsl
@@ -10,25 +10,25 @@ vec2 distort(vec2 tc, vec2 offset) {
 	return tc + offset;
 }
 
-float filter_image(vec2 offset, uint channel) {
-	float result = 0;
+vec3 filter_image_v3(vec2 offset) {
+	vec3 result = vec3(0.);
 	vec2 filter_width = .5 / res;
 	for (int y = -1; y <= 1; y++) {
 		for (int x = -1; x <= 1; x++) {
 			vec2 filter_coord = vec2(x, y);
 			float weight = exp(-(square(filter_coord.x) + square(filter_coord.y)) * .5) / (2 * Pi);
-			result += weight * textureLod(color, fract(distort(tc + filter_coord * filter_width, offset)), 0)[channel];
+			result += weight * textureLod(color, fract(distort(tc + filter_coord * filter_width, offset)), 0).rgb;
 		}
 	}
 	return result;
 }
 
 void main() {
-	vec2 red_offset = post_sep_red_offset_rt_vec2;
-	vec2 green_offset = post_sep_green_offset_rt_vec2;
-	vec2 blue_offset = post_sep_blue_offset_rt_vec2;
-	float red_offsetted = filter_image(red_offset, 0);
-	float green_offsetted = filter_image(green_offset, 1);
-	float blue_offsetted = filter_image(blue_offset, 2);
-	out_color = vec3(red_offsetted, green_offsetted, blue_offsetted);
+	vec2 c1_offset = vec2(0.);
+	vec2 c2_offset = post_hustensaft_offset_rt_vec2;
+
+	vec3 c1_hustensaft = filter_image_v3(c1_offset);
+	vec3 c2_hustensaft = filter_image_v3(c2_offset);
+
+	out_color = mix(c1_hustensaft, c2_hustensaft, post_hustensaft_color_rt_color);
 }
