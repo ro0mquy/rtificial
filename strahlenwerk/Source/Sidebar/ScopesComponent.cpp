c8a4f39d29cd31e08a03e557774fc4293e9486eb
Ps0ke <mail@ps0ke.de>
2015-11-10 01:21:33 +0100

    Make ScopesComponent listen to all sorts of events that update the rendered image.

--- strahlenwerk/Source/Sidebar/ScopesComponent.cpp
+++ strahlenwerk/Source/Sidebar/ScopesComponent.cpp
@@ -3,15 +3,24 @@
 #include <StrahlenwerkApplication.h>
 #include <MainWindow.h>
 #include <Renderer.h>
+#include <Timeline/TimelineData.h>
+#include <Timeline/CameraController.h>
+#include <AudioManager.h>
 
 ScopesComponent::ScopesComponent() :
-	renderer(nullptr)
+	renderer(nullptr),
+	data(TimelineData::getTimelineData()),
+	audioManager(AudioManager::getAudioManager())
 {
-	histogramComponent = new HistogramComponent();
+	histogramComponent = new HistogramComponent(image);
 
 	concertinaPanel = new ConcertinaPanel();
 	concertinaPanel->addPanel(0, histogramComponent, false);
 	addAndMakeVisible(concertinaPanel);
+
+	if (isVisible()) {
+		addAsListener();
+	}
 
 	triggerAsyncUpdate();
 }
@@ -25,16 +34,28 @@ void ScopesComponent::handleAsyncUpdate() {
 }
 
 ScopesComponent::~ScopesComponent() {
-	if (renderer != nullptr) {
-		renderer->removeChangeListener(this);
-	}
+	removeAsListener();
 	histogramComponent = nullptr;
 	concertinaPanel = nullptr;
 }
 
-void ScopesComponent::changeListenerCallback(ChangeBroadcaster* /*source*/) {
-	renderer->removeChangeListener(this);
-	image = renderer->getRenderedImage();
+void ScopesComponent::changeListenerCallback(ChangeBroadcaster* source) {
+	if (source == renderer) {
+		// wheee the image is ready to be taken out of the oven :o
+		renderer->removeChangeListener(this);
+		image = renderer->getRenderedImage();
+
+		const int numPanels = concertinaPanel->getNumPanels();
+		for (int i = 0; i < numPanels; i++) {
+			concertinaPanel->getPanel(i)->repaint();
+		}
+	} else {
+		// time or data changed, therefore the image was probably rerendered
+		if (isVisible()) {
+			renderer->addChangeListener(this);
+			renderer->requestRenderedImage();
+		}
+	}
 }
 
 void ScopesComponent::resized() {
@@ -42,22 +63,76 @@ void ScopesComponent::resized() {
 }
 
 void ScopesComponent::visibilityChanged() {
-	if (renderer != nullptr) {
-		if (isVisible()) {
+	if (isVisible()) {
+		if (renderer != nullptr) {
 			renderer->addChangeListener(this);
 			renderer->requestRenderedImage();
-		} else {
-			renderer->removeChangeListener(this);
 		}
+		addAsListener();
+	} else {
+		removeAsListener();
+	}
+}
+
+// ValueTree::Listener callbacks
+void ScopesComponent::valueTreePropertyChanged(ValueTree& /*parentTree*/, const Identifier& /*property*/) {
+	if (isVisible()) {
+		renderer->addChangeListener(this);
+		renderer->requestRenderedImage();
+	}
+}
+
+void ScopesComponent::valueTreeChildAdded(ValueTree& /*parentTree*/, ValueTree& /*childWhichHasBeenAdded*/) {
+	if (isVisible()) {
+		renderer->addChangeListener(this);
+		renderer->requestRenderedImage();
+	}
+}
+
+void ScopesComponent::valueTreeChildRemoved(ValueTree& /*parentTree*/, ValueTree& /*childWhichHasBeenRemoved*/, int /*indexFromWhichChildWasRemoved*/) {
+	if (isVisible()) {
+		renderer->addChangeListener(this);
+		renderer->requestRenderedImage();
+	}
+}
+
+void ScopesComponent::valueTreeChildOrderChanged(ValueTree& /*parentTree*/, int /*oldIndex*/, int /*newIndex*/) {
+}
+
+void ScopesComponent::valueTreeParentChanged(ValueTree& /*treeWhoseParentHasChanged*/) {
+}
+
+void ScopesComponent::addAsListener() {
+	if (renderer != nullptr) {
+		renderer->addChangeListener(this);
+	}
+	data.addListenerToTree(this);
+	audioManager.addChangeListener(this);
+	if (CameraController::globalCameraController != nullptr) {
+		CameraController::globalCameraController->addChangeListener(this);
+	}
+}
+
+void ScopesComponent::removeAsListener() {
+	if (renderer != nullptr) {
+		renderer->removeChangeListener(this);
+	}
+	data.removeListenerFromTree(this);
+	audioManager.removeChangeListener(this);
+	if (CameraController::globalCameraController != nullptr) {
+		CameraController::globalCameraController->removeChangeListener(this);
 	}
 }
 
 
-HistogramComponent::HistogramComponent() {
+HistogramComponent::HistogramComponent(Image& image_) :
+	image(image_)
+{
 	setName("Histogram");
 	setBufferedToImage(true);
 }
 
 void HistogramComponent::paint(Graphics& g) {
-	g.fillAll(Colours::red);
+	g.fillAll(image.getPixelAt(1,1));
 }
+
--- strahlenwerk/Source/Sidebar/ScopesComponent.h
+++ strahlenwerk/Source/Sidebar/ScopesComponent.h
@@ -4,12 +4,15 @@
 #include <juce>
 
 class Renderer;
+class TimelineData;
+class AudioManager;
 
 class HistogramComponent;
 
 class ScopesComponent :
 	public Component,
 	private ChangeListener,
+	private ValueTree::Listener,
 	private AsyncUpdater
 {
 	public:
@@ -17,6 +20,11 @@ class ScopesComponent :
 		~ScopesComponent();
 
 		void changeListenerCallback(ChangeBroadcaster* /*source*/) override;
+		void valueTreePropertyChanged(ValueTree& /*parentTree*/, const Identifier& /*property*/) override;
+		void valueTreeChildAdded(ValueTree& /*parentTree*/, ValueTree& /*childWhichHasBeenAdded*/) override;
+		void valueTreeChildRemoved(ValueTree& /*parentTree*/, ValueTree& /*childWhichHasBeenRemoved*/, int /*indexFromWhichChildWasRemoved*/) override;
+		void valueTreeChildOrderChanged(ValueTree& /*parentTree*/, int /*oldIndex*/, int /*newIndex*/) override;
+		void valueTreeParentChanged(ValueTree& /*treeWhoseParentHasChanged*/) override;
 
 	private:
 		void resized() override;
@@ -24,7 +32,12 @@ class ScopesComponent :
 
 		void handleAsyncUpdate() override;
 
+		void addAsListener();
+		void removeAsListener();
+
 		Renderer* renderer;
+		TimelineData& data;
+		AudioManager& audioManager;
 
 		ScopedPointer<ConcertinaPanel> concertinaPanel;
 		ScopedPointer<HistogramComponent> histogramComponent;
@@ -38,10 +51,12 @@ class HistogramComponent :
 	public Component
 {
 	public:
-		HistogramComponent();
+		HistogramComponent(Image& image_);
 
 	private:
 		void paint(Graphics& g) override;
+
+		Image& image;
 
 		JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(HistogramComponent)
 };
