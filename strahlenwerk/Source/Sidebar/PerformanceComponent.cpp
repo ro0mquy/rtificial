780d97a59070c861e905f72e7767064363b5111e
ro0mquy <git@ro0mquy.de>
2015-11-26 23:57:01 +0100

    add total duration to performance inspector

--- strahlenwerk/Source/Sidebar/PerformanceComponent.cpp
+++ strahlenwerk/Source/Sidebar/PerformanceComponent.cpp
@@ -13,10 +13,13 @@ PerformanceComponent::~PerformanceComponent() {
 
 void PerformanceComponent::paint(Graphics& g) {
 	const performance_log_t& performanceLog = StrahlenwerkApplication::getInstance()->getProject().getPerformanceLog();
-	String log = "";
+	String log;
+	uint64_t totalDuration = 0;
 	for (const std::pair<std::string, uint64_t>& logEntry : performanceLog) {
 		log += logEntry.first + ": " + std::to_string(logEntry.second / 1000) + " µs\n";
+		totalDuration += logEntry.second;
 	}
+	log += "\ntotal: " + std::to_string(totalDuration / 1000) + " µs";
 	g.setFont(Font(Font::getDefaultMonospacedFontName(), 13, Font::plain));
 	g.drawMultiLineText(log, 20, 20, getWidth() - 40);
 }
