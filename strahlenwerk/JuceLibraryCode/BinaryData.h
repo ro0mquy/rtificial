8d7096a27b63f8855a15ef8b5c636758a1ef228a
ro0mquy <git@ro0mquy.de>
2015-12-20 16:34:50 +0100

    remove LabColor files and fix some other introjucer stuff

--- strahlenwerk/JuceLibraryCode/BinaryData.h
+++ strahlenwerk/JuceLibraryCode/BinaryData.h
@@ -9,9 +9,6 @@
 
 namespace BinaryData
 {
-    extern const char*   juce;
-    const int            juceSize = 43;
-
     extern const char*   dejavu_sans_book_ttf;
     const int            dejavu_sans_book_ttfSize = 5657344;
 
@@ -19,7 +16,7 @@ namespace BinaryData
     extern const char* namedResourceList[];
 
     // Number of elements in the namedResourceList array.
-    const int namedResourceListSize = 2;
+    const int namedResourceListSize = 1;
 
     // If you provide the name of one of the binary resource variables above, this function will
     // return the corresponding data and its size (or a null pointer if the name isn't found).
