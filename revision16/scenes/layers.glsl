53ef91fc50a1947bdefcaebaf9f2eed11c2cdab9
ro0mquy <git@ro0mquy.de>
2016-01-30 18:05:56 +0100

    add guards to layer effect

--- revision16/scenes/layers.glsl
+++ revision16/scenes/layers.glsl
@@ -18,15 +18,97 @@ float fLayer(vec2 p, float t) {
 MatWrap wLayerEffect(vec3 p) {
 	float i_layer = pDomrepInterval(p.z, lay_layer_dist_rt_float, -lay_last_layer_index_rt_float, 0);
 	float t = i_layer;
+	float f = Inf;
 	if (i_layer == 0) {
 		t += lay_animation_rt_float;
+
+		// main
+		float pz_main = p.z;
+		pMirrorTrans(pz_main, lay_layer_thickness_rt_float);
+		float f_layer_main = fLayer(p.xy, t);
+		f_layer_main = max(f_layer_main, pz_main);
+
+		// guard after
+		float pz_after = p.z;
+		pTrans(pz_after, -(lay_layer_dist_rt_float - 2. * lay_layer_thickness_rt_float));
+		//float f_layer_after = fLayer(p.xy, floor(t - 1));
+		//f_layer_after = max(f_layer_after, pz_after);
+		float f_layer_after = pz_after;
+
+		f = f_layer_main;
+		f = min(f, f_layer_after);
+	} else if (i_layer == 1) {
+		float t_before = t + lay_animation_rt_float + 1;
+		t += floor(lay_animation_rt_float);
+
+		// main
+		float pz_main = p.z;
+		pMirrorTrans(pz_main, lay_layer_thickness_rt_float);
+		float f_layer_main = fLayer(p.xy, t);
+		f_layer_main = max(f_layer_main, pz_main);
+
+		// guard before
+		float pz_before = p.z;
+		pTrans(pz_before, lay_layer_dist_rt_float - 2. * lay_layer_thickness_rt_float);
+		//float f_layer_before = fLayer(p.xy, t_before);
+		//f_layer_before = max(f_layer_before, -pz_before);
+		float f_layer_before = -pz_before;
+
+		// guard after
+		float pz_after = p.z;
+		pTrans(pz_after, -(lay_layer_dist_rt_float - 2. * lay_layer_thickness_rt_float));
+		//float f_layer_after = fLayer(p.xy, t - 1);
+		//f_layer_after = max(f_layer_after, pz_after);
+		float f_layer_after = pz_after;
+
+		f = f_layer_main;
+		f = min(f, f_layer_before);
+		f = min(f, f_layer_after);
+	} else if (i_layer == -lay_last_layer_index_rt_float) {
+		t += floor(lay_animation_rt_float);
+
+		// main
+		float pz_main = p.z;
+		pMirrorTrans(pz_main, lay_layer_thickness_rt_float);
+		float f_layer_main = fLayer(p.xy, t);
+		f_layer_main = max(f_layer_main, pz_main);
+
+		// guard before
+		float pz_before = p.z;
+		pTrans(pz_before, lay_layer_dist_rt_float - 2. * lay_layer_thickness_rt_float);
+		//float f_layer_before = fLayer(p.xy, t + 1);
+		//f_layer_before = max(f_layer_before, -pz_before);
+		float f_layer_before = -pz_before;
+
+		f = f_layer_main;
+		f = min(f, f_layer_before);
 	} else {
 		t += floor(lay_animation_rt_float);
-	}
-	float f_layer = fLayer(p.xy, t);
 
-	pMirrorTrans(p.z, lay_layer_thickness_rt_float);
-	float f = max(f_layer, p.z);
+		// main
+		float pz_main = p.z;
+		pMirrorTrans(pz_main, lay_layer_thickness_rt_float);
+		float f_layer_main = fLayer(p.xy, t);
+		f_layer_main = max(f_layer_main, pz_main);
+
+		// guard before
+		float pz_before = p.z;
+		pTrans(pz_before, lay_layer_dist_rt_float - 2. * lay_layer_thickness_rt_float);
+		//float f_layer_before = fLayer(p.xy, t + 1);
+		//f_layer_before = max(f_layer_before, -pz_before);
+		float f_layer_before = -pz_before;
+
+		// guard after
+		float pz_after = p.z;
+		pTrans(pz_after, -(lay_layer_dist_rt_float - 2. * lay_layer_thickness_rt_float));
+		//float f_layer_after = fLayer(p.xy, t - 1);
+		//f_layer_after = max(f_layer_after, pz_after);
+		float f_layer_after = pz_after;
+
+		f = f_layer_main;
+		f = min(f, f_layer_before);
+		f = min(f, f_layer_after);
+	}
 	MaterialId m = MaterialId(id_layer, p, vec4(t, vec3(0)));
 	return MatWrap(f, m);
 }
