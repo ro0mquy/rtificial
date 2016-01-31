d5968ebcfa0cb3ae350232fc3273c6e2f2d5c4a5
Ps0ke <mail@ps0ke.de>
2016-01-31 03:06:34 +0100

    Add flammenmeer.

--- revision16/scenes/flames.glsl
+++ revision16/scenes/flames.glsl
@@ -3,15 +3,54 @@
 #line 4
 
 
-float fFlame(vec2 p, float t, float r, float curve, float freq_t, float freq_y, float amp_x, float amp_curve) {
-	float y = clamp(p.y, -r, r) / r;
-	p.x += sin(t/20 * freq_t * Tau + y * freq_y * Tau) * pow(abs(y * amp_curve), curve) * amp_x * 2 / r;
+float fFlame(vec2 p, float t, float r, float freq_t, float freq_y, float phase, float amp_x, float curve) {
+	pTrans(p.y, .5*r);
+	float h = 1.5*r;
+	float y = clamp((p.y + .5*r) / h, 0, 1);
+	p.x += sin(t/20 * freq_t * Tau  +  y * freq_y * Tau + phase) * pow(y, curve) * amp_x * r * sqrt(3)/2;
 	return f2Triprism(p, r);
 }
 
 float fInner(vec2 p, float t) {
-	pTrans(p, 0, -lay_frame_dim.y);
-	return fFlame(p, t, 2, 0.9, 2, 0.25, 1, 1);
+	pTrans(p, 0, -lay_frame_dim.y-.5);
+	pTrans(p, vec2(cos(t/50. * 1.5 * Tau) * 0.7, sin(t/50. * 2 * Tau)) * 0.4);
+
+	vec2 p1 = p;
+	float f1 = fFlame(p1, t, 3, 2*flame_speed_rt_float, 0.3, 0, 0.8, 1.5);
+
+	vec2 p2 = p;
+	pTrans(p2, 7, 0);
+	float f2 = fFlame(p2, t, 4, 3*flame_speed_rt_float, 0.2, 1, 0.8, 1.2);
+
+	vec2 p3 = p;
+	pTrans(p3, -6, 0);
+	float f3 = fFlame(p3, t, 3.8, 2.5*flame_speed_rt_float, 0.4, 1.5, 0.7, 1.8);
+
+	vec2 p4 = p;
+	pTrans(p4, -3, 0);
+	float f4 = fFlame(p4, t, 4.1, 2.8*flame_speed_rt_float, 0.42, 0.3, 0.9, 1.9);
+
+	vec2 p5 = p;
+	pTrans(p5, 3.5, 0);
+	float f5 = fFlame(p5, t, 2.8, 2.2*flame_speed_rt_float, 0.38, 0.28, 0.85, 1.8);
+
+	vec2 p6 = p;
+	pTrans(p6, 2.5, 0);
+	float f6 = fFlame(p6, t, 1, 3.2*flame_speed_rt_float, 0.2, 0.28, 0.9, 2);
+
+	vec2 p7 = p;
+	pTrans(p7, .7, 0);
+	float f7 = fFlame(p7, t, 1.5, 2.2*flame_speed_rt_float, 0.3, 0.342, 0.85, 2);
+
+	vec2 p8 = p;
+	pTrans(p8, -3.8, 0);
+	float f8 = fFlame(p8, t, 2.3, 1.6*flame_speed_rt_float, 0.4, 1.2, 0.8, 2.1);
+
+	vec2 p9 = p;
+	pTrans(p9, -5.2, 0);
+	float f9 = fFlame(p9, t, 1.4, 1.9*flame_speed_rt_float, 0.6, 0.8, 0.75, 2.3);
+
+	return max(-min(min(f6, f7), min(f8, f9)), min(min(min(f1, f2), min(f3, f4)), f5));
 }
 
 float fScene(vec3 p) {
