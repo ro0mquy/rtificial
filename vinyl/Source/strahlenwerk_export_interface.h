7f629f62e52816747c696ce3f4d3a090f38fbf13
Vincent Schüßler <v.schuessler@gmail.com>
2016-03-23 12:32:20 +0100

    Test export

--- vinyl/Source/strahlenwerk_export_interface.h
+++ vinyl/Source/strahlenwerk_export_interface.h
@@ -7,7 +7,7 @@
 #include "Uniform.h"
 #include "AmbientLight.h"
 #include "Texture.h"
-extern Shader postproc[26];
+extern Shader postproc[23];
 extern Input scale_inputs[2];
 extern Input ao_inputs[1];
 extern Input ao_blur_h_inputs[2];
@@ -29,28 +29,25 @@ extern Input bloom_up_3rd_inputs[2];
 extern Input bloom_up_2nd_inputs[2];
 extern Input bloom_up_1st_inputs[2];
 extern Input bloom_up_final_inputs[2];
-extern Input godrays_inputs[1];
 extern Input final_inputs[1];
-extern Input colorseparation_inputs[1];
-extern Input scanlines_inputs[1];
 extern Input output_inputs[1];
-extern Shader scenes[3];
-extern AmbientLight environments[3];
-extern Texture textures[4];
-extern Framebuffer fbos[26];
-extern Scene scenes_data[4];
-extern float keyframe_data[1680];
-extern Sequence sequence_data[422];
-extern int sequence_index[177];
-extern int keyframe_index[177];
-extern Uniform uniforms[176];
-extern int keyframe_time[1014];
-extern int keyframe_ease_toward[1014];
-extern int keyframe_ease_away[1014];
-extern int keyframe_time_index[177];
-const int time_uniform_location = 67;
+extern Shader scenes[22];
+extern AmbientLight environments[22];
+extern Texture textures[6];
+extern Framebuffer fbos[23];
+extern Scene scenes_data[11];
+extern float keyframe_data[403];
+extern Sequence sequence_data[61];
+extern int sequence_index[116];
+extern int keyframe_index[116];
+extern Uniform uniforms[115];
+extern int keyframe_time[126];
+extern int keyframe_ease_toward[126];
+extern int keyframe_ease_away[126];
+extern int keyframe_time_index[116];
+const int time_uniform_location = 66;
 #ifndef BPM
-#define BPM 125
+#define BPM 130
 #endif
 const unsigned int textureOffsetAmbient = 20;
 const unsigned int textureOffsetPostproc = 25;
