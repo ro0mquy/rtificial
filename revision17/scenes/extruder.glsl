593d3be5db5b0695e3bf22161997b5635ed7f31b
Ps0ke <mail@ps0ke.de>
2017-01-08 15:44:11 +0100

    Colorize extruder.

--- revision17/scenes/extruder.glsl
+++ revision17/scenes/extruder.glsl
@@ -1,6 +1,9 @@
 #include "march.glsl"
 #line 3
 
+const float mat_id_bounding = 0.;
+const float mat_id_ext = 1.;
+
 float myTorusTwisted(vec3 p, float rBig, float rSmall) {
 	vec2 q = vec2(f2Sphere(p.xz, rBig), p.y);
 	float angle = atan(p.z, p.x);
@@ -37,21 +40,23 @@ float fScene(vec3 p) {
 	float ext_obj_loco_rt_float = ext_obj_loco_rt_float * sin(4. * (px_before + ext_rot_rt_float) * Tau);
 	// */
 
-	pMirrorLoco(p_torus.xy, vec2(ext_obj_loco_rt_float));
+	vec3 loco_index = pMirrorLoco(p_torus.xy, vec2(ext_obj_loco_rt_float));
 	pRotY(p_torus, ext_obj_rot_rt_float * Tau);
 	float f_torus = myTorusPartial(p_torus.yxz, 3., 1., ext_torus_angle_rt_float * Tau);
 
+	MatWrap w_ext = MatWrap(f_torus, MaterialId(mat_id_ext, p_torus, vec4(loco_index, px_before)));
 
-	float f = f_torus;
+	MatWrap w_bound = MatWrap(-fSphere(p, 100.), newMaterialId(mat_id_bounding, p));
+	MatWrap w = mUnion(w_ext, w_bound);
 
-	mUnion(f, newMaterialId(0., p));
-	return f;
+	mUnion(w);
+	return w.f;
 }
 
 vec3 applyLights(vec3 origin, float marched, vec3 direction, vec3 hit, vec3 normal, MaterialId materialId, Material material) {
 	vec3 emission = material.emission;
-	return applyNormalLights(origin, marched, direction, hit, normal, material) + emission;
-	//return ambientColor(normal, -direction, material) + emission;
+	//return applyNormalLights(origin, marched, direction, hit, normal, material) + emission;
+	return ambientColor(normal, -direction, material) + emission;
 }
 
 vec3 applyAfterEffects(vec3 origin, float marched, vec3 direction, vec3 color) {
@@ -60,6 +65,23 @@ vec3 applyAfterEffects(vec3 origin, float marched, vec3 direction, vec3 color) {
 
 Material getMaterial(MaterialId materialId) {
 	Material mat = defaultMaterial(vec3(1));
-	mat.roughness = .1;
+
+	if (materialId.id == mat_id_bounding) {
+		mat.color = ext_color_background_rt_color;
+	} else if (materialId.id == mat_id_ext) {
+		vec3 loco_index = (materialId.misc.xyz + 1.) / 2.; // {0, 1}
+		float px_before = (materialId.misc.w/ext_extrude_h_rt_float/ext_extrude_freq_rt_float + 1.) / 2.; // {0,1}
+
+		mat.color = ext_color_torus_rt_color;
+
+		if (loco_index.x > 0) {
+			mat.color = ext_color_torus_hightlight_rt_color;
+		}
+		if (loco_index.y > 0) {
+			mat.color += 0.5;
+		}
+		mat.color = mix(ext_color_fade_rt_color,mat.color, pow(px_before, ext_fade_pow_rt_float));
+	}
+
 	return mat;
 }
