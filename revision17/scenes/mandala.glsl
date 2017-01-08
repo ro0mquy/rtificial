b7bae852b9a5d596b359cd7b98730c6ac34bf9fc
ro0mquy <git@ro0mquy.de>
2017-01-08 13:41:49 +0100

    rename cyc uniforms to mand uniforms

--- revision17/scenes/mandala.glsl
+++ revision17/scenes/mandala.glsl
@@ -2,7 +2,7 @@
 #line 3
 
 const float mat_id_plane = 0.;
-const float mat_id_cyc = 1.;
+const float mat_id_cyl = 1.;
 
 float pDomrepGridOnlyX(inout vec2 p, float c) {
 	float i = pDomrepMirror(p.x, c);
@@ -17,38 +17,37 @@ float fScene(vec3 p) {
 	// domain geschwurbel
 	vec3 p_cyl = p;
 
-	pRotY(p_cyl, Tau * cyc_rot_global_rt_float);
+	pRotY(p_cyl, Tau * mand_rot_global_rt_float);
 
 	// domrep angle
-	if (cyc_mix_circle_enable_rt_bool) {
-		pTrans(p_cyl.z, - cyc_domrep_angle_r_rt_float / cyc_mix_circle_rt_float);
+	if (mand_mix_circle_enable_rt_bool) {
+		pTrans(p_cyl.z, - mand_domrep_angle_r_rt_float / mand_mix_circle_rt_float);
 	}
-	float c_circle = pDomrepAngle(p_cyl.zx, cyc_domrep_angle_num_rt_float * cyc_mix_circle_rt_float, cyc_domrep_angle_r_rt_float / cyc_mix_circle_rt_float);
+	float c_circle = pDomrepAngle(p_cyl.zx, mand_domrep_angle_num_rt_float * mand_mix_circle_rt_float, mand_domrep_angle_r_rt_float / mand_mix_circle_rt_float);
 
 	pTrans(p_cyl.x, 4.);
 
 	// grid domrep
 	vec3 q_grid = p_cyl;
 	float c_grid = pDomrepGridOnlyX(p_cyl.xz, 8.);
-	p_cyl = mix(q_grid, p_cyl, cyc_mix_grid_rt_float);
+	p_cyl = mix(q_grid, p_cyl, mand_mix_grid_rt_float);
 
-	pRotY(p_cyl, Tau * cyc_rot_cell_rt_float);
-	pTrans(p_cyl.xz, cyc_trans_inner_rt_vec2);
+	pRotY(p_cyl, Tau * mand_rot_cell_rt_float);
+	pTrans(p_cyl.xz, mand_trans_inner_rt_vec2);
 
 	// distance function geschwurbel
 	float f_cyl = f2Box(p_cyl.zx, 1., 2.*Golden_Ratio);
-	float c_distfunc = pDomrepInterval(f_cyl, cyc_f_domrep_rt_float, 0., 1.);
-	f_cyl = f2Box(vec2(f_cyl, p_cyl.y), cyc_f_thickness_rt_float);
+	float c_distfunc = pDomrepInterval(f_cyl, mand_f_domrep_rt_float, 0., 1.);
+	f_cyl = f2Box(vec2(f_cyl, p_cyl.y), mand_f_thickness_rt_float);
 
 	// material
-	MatWrap w_cyl = MatWrap(f_cyl, MaterialId(mat_id_cyc, p_cyl, vec4(c_circle, c_grid, c_distfunc, 0.)));
+	MatWrap w_cyl = MatWrap(f_cyl, MaterialId(mat_id_cyl, p_cyl, vec4(c_circle, c_grid, c_distfunc, 0.)));
 
 	// ground plane
 	float f_plane = p.y;
 	MatWrap w_plane = MatWrap(f_plane, newMaterialId(mat_id_plane, p));
 
 	MatWrap w = mUnion(w_cyl, w_plane);
-	w = w_cyl;
 	mUnion(w);
 	return w.f;
 }
@@ -68,17 +67,17 @@ Material getMaterial(MaterialId materialId) {
 	mat.roughness = 0.1;
 
 	if (materialId.id == mat_id_plane) {
-		mat.color = cyc_color_ground_rt_color;
+		mat.color = mand_color_ground_rt_color;
 		mat.roughness = 1.;
-	} else if (materialId.id == mat_id_cyc) {
-		float c_circle = (materialId.misc.x + floor(cyc_domrep_angle_num_rt_float/2.));// / (cyc_domrep_angle_num_rt_float-1);
+	} else if (materialId.id == mat_id_cyl) {
+		float c_circle = (materialId.misc.x + floor(mand_domrep_angle_num_rt_float/2.));// / (mand_domrep_angle_num_rt_float-1);
 		float c_grid = materialId.misc.y / 2. + .5; // 0,1
 		float c_distfunc = materialId.misc.z; // null oder 1 nicer wert
 
 		if (c_distfunc > 0) {
-			mat.color = 2* cyc_color_circle_rt_color;
+			mat.color = 2* mand_color_circle_rt_color;
 		} else {
-			mat.color = cyc_color_2_rt_color;
+			mat.color = mand_color_2_rt_color;
 		}
 		if (c_grid > 0) {
 			mat.color += 0.5;
