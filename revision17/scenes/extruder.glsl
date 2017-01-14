8333fdcaf887ce4c0cbc750e4917baf77c317092
ro0mquy <git@ro0mquy.de>
2017-01-15 00:30:32 +0100

    add translation parameter to extruder

--- revision17/scenes/extruder.glsl
+++ revision17/scenes/extruder.glsl
@@ -28,20 +28,23 @@ float myTorusPartial(vec3 p, float rBig, float rSmall, float halfAngle) {
 float fScene(vec3 p) {
 	vec3 p_torus = p;
 
-	//*
 	pTrans(p_torus.x, -ext_extrude_h_rt_float);
-	float px_before = p_torus.x * ext_extrude_freq_rt_float;
-	p_torus.x -= clamp(p_torus.x, -ext_extrude_h_rt_float, ext_extrude_h_rt_float);
+	float px_before = p_torus.x;
+	float px_clamped = clamp(p_torus.x, -ext_extrude_h_rt_float, ext_extrude_h_rt_float);
+	p_torus.x -= px_clamped;
 
-	pRotX(p_torus, 2. * (px_before + ext_rot_rt_float) * Tau);
-	pRotY(p_torus,      (px_before + ext_rot_rt_float) * Tau);
-	pRotZ(p_torus, 4. * (px_before + ext_rot_rt_float) * Tau);
+	float px_param = px_clamped * ext_extrude_freq_rt_float;
 
-	float ext_obj_loco_rt_float = ext_obj_loco_rt_float * sin(4. * (px_before + ext_rot_rt_float) * Tau);
-	// */
+	pRotX(p_torus, 2. * (px_param + ext_rot_rt_float) * Tau);
+	pTrans(p_torus.z, 10 * sin((5. * px_param + ext_trans_rt_float) * Tau));
+	pRotY(p_torus,      (px_param + ext_rot_rt_float) * Tau);
+	pRotZ(p_torus, 4. * (px_param + ext_rot_rt_float) * Tau);
 
-	vec3 loco_index = pMirrorLoco(p_torus.xy, vec2(ext_obj_loco_rt_float));
+	vec2 loco_torus = vec2(ext_obj_loco_rt_float * sin(4. * (px_param + ext_rot_rt_float) * Tau));
+
+	vec3 loco_index = pMirrorLoco(p_torus.xy, loco_torus);
 	pRotY(p_torus, ext_obj_rot_rt_float * Tau);
+
 	float f_torus = myTorusPartial(p_torus.yxz, 3., 1., ext_torus_angle_rt_float * Tau);
 
 	MatWrap w_ext = MatWrap(f_torus, MaterialId(mat_id_ext, p_torus, vec4(loco_index, px_before)));
@@ -70,7 +73,7 @@ Material getMaterial(MaterialId materialId) {
 		mat.color = ext_color_background_rt_color;
 	} else if (materialId.id == mat_id_ext) {
 		vec3 loco_index = (materialId.misc.xyz + 1.) / 2.; // {0, 1}
-		float px_before = (materialId.misc.w/ext_extrude_h_rt_float/ext_extrude_freq_rt_float + 1.) / 2.; // {0,1}
+		float px_before = (materialId.misc.w/ext_extrude_h_rt_float + 1.) / 2.; // {0,1}
 
 		mat.color = ext_color_torus_rt_color;
 
